6bd13b9737329e732f18b0d21768574627540b18
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-15 14:54:17 +0200

    update

---
+++ src/VariableDeclarations.java
@@ -0,0 +1,15 @@
+/**
+*   Mit Variablen Daten speichern, die vom Programm gelesen und geschrieben werden können.
+ * - Variablendeklarationen
+ * - Mehrere Variablen kompakt deklarieren
+ * - Keine automatische Initialisierung von lokalen Variablen
+ * - Variablendeklaration mit Wert initialisierung
+ * - Finale Variablen
+ * - char und String
+ */
+
+public class VariableDeclarations {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
