7b0b779ce84d334cddcbe8328574b4951355dfe2
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-15 14:14:43 +0200

    update

--- src/Literals.java
+++ src/Literals.java
@@ -2,5 +2,10 @@ public class Literals {
     public static void main(String[] args) {
         System.out.println(true);
         System.out.println(false);
+        System.out.println(1233);
+        System.out.println(1.765);
+        System.out.println(10E1); // 10 hoch 1 = 100
+        System.out.println(0.1 + 0.1); // = 0.2
+        System.out.println(0.1 + 0.1 + 0.1); // = etwas ungenau 0.30000000000000004
     }
 }
