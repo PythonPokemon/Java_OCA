234647ea8b3c46be2faa21f781d7da136b9b2b73
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-15 14:49:26 +0200

    update

---
+++ src/Types.java
@@ -0,0 +1,25 @@
+/**
+ * Die Datentypen in Java zerfallen in zwei Kategorien
+ *
+ * -Primitive Typen
+ *  Die primitiven (einfachen) Typen sind fest in der Sprache Java eingebaute
+ *  Datentypen für Zahlen, Unicode-Zeichen und Wahrheitswerte.
+ *
+ * -Referenztypen:
+ *  Mit diesem Datentyp lassen sich Objektweise etwa auf Zeichenketten,
+ *  Datenstrukturen oder Zwergpinscher verwalten.
+ *
+ * In Java gibt es zwei Arten eingebauter primitiver Datentypen:
+ *
+ * -arythmetische Typen
+ * 1. Gazte Zahlen, 2. Fließkommazahlen, 3. Unicode-Zeichen
+ *
+ * -Wahrheitswerte
+ *  für die Zustände wahr und Falsch
+ */
+
+public class Types {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
