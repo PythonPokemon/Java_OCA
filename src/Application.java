ce05879708e3b74594df1fbec445962766651801
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-15 10:59:07 +0200

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,17 @@
+/**
+ * Lernziel:
+ * - Klassen > Methoden > Code
+ * - Main (...)
+ * - Bezeichner
+ * - Groß-/Kleinschreibung
+ * - Rservierte Schlüsselwörter
+ * - Modifizierer
+ * - Einrücken
+ */
+
+class Application {
+    // das ist eine Methode
+    public static void main(String[] args) {
+        System.out.println("Hallo Welt");
+    }
+}
