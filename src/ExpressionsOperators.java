9f090606db1fb2c828d13e13f8cc04ac9159c75a
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-16 11:28:28 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/VariableDeclarations.java

--- src/ExpressionsOperators.java
+++ src/ExpressionsOperators.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 /**
  * Ein *Ausdruck* ( *expression*) ergibt bei der Auswertung ein Ergebnis.
  * Lernziel:
@@ -22,29 +24,27 @@ public class ExpressionsOperators {
         int age;  // zuweisung
         System.out.println(age = 90); // initialisierung
         System.out.println(age);
-
         System.out.println(Math.random()); // 0.0 < random < 1.0
         System.out.println(-Math.random()); // -0.0 < random < -1.0
         System.out.println(-(-Math.random())); // 0.0 < random < 1.0
-
         System.out.println(Math.random() * 10); // 0.0 < random < 10.0
 
         int number = 10_000_000;
         System.out.println(10_000_000L * number); // 10_000_000L = Long statt int da die Bytes zu wenig speicher haben für so eine große zahl, würde falsches Ergebnis liefern während laufzeitentwicklung
-        System.out.println(10.0/3); // division = 3,3333
-        System.out.println(10%3); // Modulo = 1 | 10 geteilt durch 3 Rest 1
+        System.out.println(10.0 / 3); // division = 3,3333
+        System.out.println(10 % 3); // Modulo = 1 | 10 geteilt durch 3 Rest 1
 
         int b1 = 0b10101010;
         int b2 = 0b11010100;
-        System.out.println(Integer.toBinaryString(b1) );
-        System.out.println(Integer.toBinaryString(b2) );
+        System.out.println(Integer.toBinaryString(b1));
+        System.out.println(Integer.toBinaryString(b2));
 
-        System.out.println(Integer.toBinaryString(~b1) );
-        System.out.println(Integer.toBinaryString(~b2) );
+        System.out.println(Integer.toBinaryString(~b1));
+        System.out.println(Integer.toBinaryString(~b2));
 
-        System.out.println(Integer.toBinaryString( b1 & b2) );
-        System.out.println(Integer.toBinaryString( b1 | b2) );
-        System.out.println(Integer.toBinaryString( b1 ^ b2) );
+        System.out.println(Integer.toBinaryString(b1 & b2));
+        System.out.println(Integer.toBinaryString(b1 | b2));
+        System.out.println(Integer.toBinaryString(b1 ^ b2));
 
         System.out.println(1 < 2);  // 1 ist kleiner 2   = True
         System.out.println(1 > 0);  // 1 ist größer 0    = True
@@ -80,5 +80,39 @@ public class ExpressionsOperators {
         System.out.println("Negierte bsp. Rechnung");
         boolean a = 10 * 10 < 99;   // 10 * 10 = 100 kleiner 99 | also false wenn es aber negiert wird dann true!
         System.out.println(!a);
+
+        // Kurzschluss Operanten | benutzt man wenn man das ergebnis kennt und der rest egal ist
+        System.out.println("OR ODER | Kurzschluss Operator");
+        System.out.println(true || true || false);
+
+        System.out.println("AND UND | Kurzschluss Operator");
+        System.out.println(false && true && true);
+
+        // Nicht Kurzschluss Operanten | benutzt man, wenn man das Ergebnis kennt aber der Rest noch mitgezählt werden soll, kann unerwünschte effekte haben
+        System.out.println("Nicht Kurzschlus Operator");
+        System.out.println(true | true | false); // Oder
+        System.out.println(false & true & true); // UND
+
+        // Abkürzung Syntax
+        System.out.println("Syntax abkürzung");
+        double random = Math.random();
+        random = random * 10; // variante 1
+        //  random *= 10; variante 2
+        //  random ++; variante 3 | präfix kann auch vorne stehen ++random;
+        System.out.println(random);
+
+        // Punkt vor Strich rechnung und kombination string + Int
+        System.out.println("Punkt vo Strich Rechnung bsp. 1 + 1 * 10 = " + (1+1)*10);
+        System.out.println("hallo " + "Ich bin der " + 1 + " Elden " + "Lord");
+
+        // alternative
+        int nn = 99;
+        String hello = "Hallo ";
+        System.out.println(hello + nn + " Welten");
+        System.out.println(1+1+"2"+3+4); // compiler liest von links nach rechts = 2234
+
+        // ganze funktion in eine string packen und Initialisieren
+        String s = "Zufallszahl " + Math.random();
+        System.out.println(s);
     }
 }
