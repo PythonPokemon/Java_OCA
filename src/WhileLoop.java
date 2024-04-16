a01ae60855afdea32affbe0e1c91af99a3b99726
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-16 20:14:15 +0200

    update

--- src/WhileLoop.java
+++ src/WhileLoop.java
@@ -1,15 +1,36 @@
 /**
  * Lernziel:
- * - while schleife = Kopfgesteuerte schleife, prüft zunächst ob bedingung gilt, wenn ja wird diese durchlauf und das bei jedem durchlauf
+ * - while schleife = Kopfgesteuerte schleife, prüft zunächst ob bedingung gilt, wenn ja wird diese durchlaufen und das bei jedem durchlauf
  * - Endlosschleife = wenn die bedingung true ist | lassen sich mit break beenden | oder mit continue fortsetzen
  * - break und continue
- *
+ * <p>
  * Syntax:
  * while ( Bedingung ) Anweisung
  */
 
 public class WhileLoop {
     public static void main(String[] args) {
+//    while (Math.random() > 0.1)
+//        System.out.println("Rumpf");
 
+//        // Variante 1 | Zum Testen Auskommentieren |
+//        int number = 1234567;               // ganzzahl number anfangswert zuweisung = 1234567 deklaration
+//        int digit = 0;                      // ganzzahl aufzählung anfangswert zuweisung = 0 deklaration
+//        while (number > 0){                 // solnage number größer 0 ist
+//            digit++;                        // führt eine adition +1 für jede weitere aufzählung
+//            number /= 10;                   // wird number durch 10 geteilt
+//            System.out.println("Anzahl Aufzählung: " + digit + " und noch teilbarer Restwert " + number);     // gibt in der konsole number aus
+//        }
+
+        // Variante 2 | Zum Testen Auskommentieren |
+        int counter = 0;
+        final int LIMIT = 100;
+        while ( Math.random() > 0.0001 ) {
+            if ( counter >= LIMIT )
+                break;
+            System.out.println("Rumpf " + counter);
+            counter++;
+        }
     }
 }
+
