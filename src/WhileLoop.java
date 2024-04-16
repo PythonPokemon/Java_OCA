5dfa5e4ea20f444b1217b0cb09c2047ee8896cbd
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-16 19:38:04 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/VariableDeclarations.java

---
+++ src/WhileLoop.java
@@ -0,0 +1,15 @@
+/**
+ * Lernziel:
+ * - while schleife = Kopfgesteuerte schleife, prüft zunächst ob bedingung gilt, wenn ja wird diese durchlauf und das bei jedem durchlauf
+ * - Endlosschleife = wenn die bedingung true ist | lassen sich mit break beenden | oder mit continue fortsetzen
+ * - break und continue
+ *
+ * Syntax:
+ * while ( Bedingung ) Anweisung
+ */
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+    }
+}
