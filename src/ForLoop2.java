bec402d43d3f2a56db83a4d7727004e53be1d8bb
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-17 12:08:08 +0200

    update

--- src/ForLoop2.java
+++ src/ForLoop2.java
@@ -8,7 +8,7 @@ public class ForLoop2 {
 
         // Variante 1
         for (int x = 0; x < 10; x++) {
-            for (int y = 0; y < 10; y++) {  // Loopl im Loop wird zuerst ausgeführt | y wird deklariert und initialisiert ; bedingung wird geprüft ob y noch kleiner 10 ist, wenn ja läuft es weiter ; der anweisung nach wird y plus 1 zu addieren
+            for (int y = 0; y < 10; y++) {  // Loop im Loop wird zuerst ausgeführt | y wird deklariert und initialisiert ; bedingung wird geprüft ob y noch kleiner 10 ist, wenn ja läuft es weiter ; der anweisung nach wird y plus 1 zu addieren
                 System.out.println("x=" + x + " y=" + y);   // Konsole gibt deklaration aus, indem falls x und y
             }
         }
