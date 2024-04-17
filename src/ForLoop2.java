12d497c8b7f03ea982a658143ab74d89c45a25ed
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-17 14:21:25 +0200

    update

--- src/ForLoop2.java
+++ src/ForLoop2.java
@@ -6,13 +6,19 @@
 public class ForLoop2 {
     public static void main(String[] args) {
 
-        // Variante 1
-        for (int x = 0; x < 10; x++) {
-            for (int y = 0; y < 10; y++) {  // Loop im Loop wird zuerst ausgeführt | y wird deklariert und initialisiert ; bedingung wird geprüft ob y noch kleiner 10 ist, wenn ja läuft es weiter ; der anweisung nach wird y plus 1 zu addieren
-                System.out.println("x=" + x + " y=" + y);   // Konsole gibt deklaration aus, indem falls x und y
-            }
-        }
+//        // Variante 1
+//        for (int x = 0; x < 10; x++) {
+//            for (int y = 0; y < 10; y++) {  // Loop im Loop wird zuerst ausgeführt | y wird deklariert und initialisiert ; bedingung wird geprüft ob y noch kleiner 10 ist, wenn ja läuft es weiter ; der anweisung nach wird y plus 1 zu addieren
+//                System.out.println("x=" + x + " y=" + y);   // Konsole gibt deklaration aus, indem falls x und y
+//            }
+//        }
 
         //Variante 2
+        for ( int x = 1; x <= 10; x++) {
+            for ( int y = 1; y <= 10; y++) {
+                System.out.printf("%3d ", x+y); // gibt konsole in spalten aus
+            }
+            System.out.println();   // gibt konsole in zeilen aus
+        }
     }
 }
---
+++ src/Methods1.java
@@ -0,0 +1,34 @@
+/**
+ * Lernziel:
+ * - Bedeutung von Methoden verstehen
+ * - Statische Methoden deklarieren
+ * - Methoden aufrufen
+ * - Deklarierte Methode und Aufruf in verschiedenen Klassen haben
+ * <p>
+ * Bps. wird benutz um code dublikationen zu vermeiden mittels copy paste von code schnipseln!
+ * = Besser eine Methode erstellen und deklarieren um daraus später werte zu extrahieren
+ *
+ * return beendet eine Methode
+ */
+
+public class Methods1 {
+
+    // Sichtbarkeit der Methode Öffentlich
+    public static void main(String[] args) {
+        Lines();
+        System.out.println();
+        Lines();
+    }
+
+    // Sichbarkeit der Methode privat
+    private static void Lines() {
+        for (int i = 0; i < 10; i++) {
+            printHartzIV();
+        }
+    }
+
+
+    static void printHartzIV() {
+        System.out.print("HartzIV ");
+    }
+}
