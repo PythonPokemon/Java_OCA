489e843e63cdde491dc5f85d52c229f3fcdbdc37
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-22 13:57:18 +0200

    update

--- src/TestAufgaben/LZK10.java
+++ src/TestAufgaben/LZK10.java
@@ -3,7 +3,7 @@ package TestAufgaben;
 * Hier wrden nur die Farben mit der Länge einer zeichenkette ungleich 4 aus
 * bedeutet red zu klein
 * green, yellow, maroon, zu groß
-* passend blue , cyan | da diese exakt 4 haben!
+* passend blue, cyan | da diese exakt 4 haben!
 * */
 public class LZK10 {
     public static void main(String[] args) {
