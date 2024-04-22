16453ce85a2d7f6d5d1280d1d35e0d95373edf9a
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-22 11:41:19 +0200

    update

---
+++ src/TestAufgaben/LZK5.java
@@ -0,0 +1,11 @@
+package TestAufgaben;
+
+public class LZK5 {
+    public static void main(String[] args) {
+
+        boolean b1 = (5.0 != 6.0) && (4 !=5); // und operation
+        boolean b2 = (4 != 4) || (4 ==4);
+        System.out.println("b1 " + b1 + "\\nb2 " + b2);
+    }
+
+}
