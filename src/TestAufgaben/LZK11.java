369a4c5b07548fff275b0bee5b3a2594a12a4770
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-22 14:25:21 +0200

    update

---
+++ src/TestAufgaben/LZK11.java
@@ -0,0 +1,10 @@
+package TestAufgaben;
+
+public class LZK11 {
+    public static void main(String[] args) {
+        int i, j = 0;
+        i = (3 * 2 + 4 + 5); // 3 * 2 = 6 + 4 + 5 = 15
+        j = (3 * (2 + 4) + 5); // 3 * 6 = 18 + 5 = 23
+        System.out.println("i: " + i + "\nj: " + j); // gibt das ergebnis von i = 15 | zeilenumbruch \n | gibt das ergebnis von j = 23
+    }
+}
