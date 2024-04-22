d7ec4eed9121d754bbe1e58e551c5fe2b21b7c5f
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-22 10:43:14 +0200

    update

--- src/OOPNew.java
+++ src/OOPNew.java
@@ -1,2 +1,11 @@
+/*
+* Lernziel:
+* - Objekte haben: Eine Identität, einen Zustand, ein Verhalten
+* - Objekte mit new erzeugen
+* - Konstruktoraufruf
+* - Referenzvariablen deklarieren
+* - Referenztyp
+* */
+
 public class OOPNew {
 }
