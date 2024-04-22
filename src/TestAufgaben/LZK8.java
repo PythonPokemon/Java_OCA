e06c79e00a9c03395510397ea9219f80429006f1
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-22 13:33:56 +0200

    update

---
+++ src/TestAufgaben/LZK8.java
@@ -0,0 +1,8 @@
+package TestAufgaben;
+
+public class LZK8 {
+    public static void main(String[] args) {
+        System.out.println("Result:"+3+5); // = 35 | da 3 und 5 wie eine zeichenkette gesehen und nicht addiert werden.
+        System.out.println("Result:"+(3+5)); // = Result: 8 | weil in klammer tatsächlich addiert wird
+    }
+}
