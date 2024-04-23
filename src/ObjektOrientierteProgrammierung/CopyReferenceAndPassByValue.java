2ed6b77715d552026083d814880e5adb4711882c
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-23 11:53:25 +0200

    update

--- src/ObjektOrientierteProgrammierung/CopyReferenceAndPassByValue.java
+++ src/ObjektOrientierteProgrammierung/CopyReferenceAndPassByValue.java
@@ -2,7 +2,7 @@ package ObjektOrientierteProgrammierung;
 
 public class CopyReferenceAndPassByValue {
 
-    // main Methode
+    // main Methode kann aus anderen methoden die werte in der eigenen main methode ausgeben
     public static void main(String[] args) {
         // Variante 1
         int age = 50;
@@ -18,15 +18,34 @@ public class CopyReferenceAndPassByValue {
         q.y = 20;
         System.out.println(p.y);
 
-        printPoint(p);
+        //------Methodenzugriffe------
+        printPoint(p);  // gibt wert ausr der Methode aus | static void printPoint(java.awt.Point p)
+
+        System.out.println(randomPoint());  // gibt die werte aus der Methode aus | static java.awt.Point randomPoint()
+
+        System.out.println(clearPoint2( p ));
+        System.out.println(p);
     }
 
     // die variable p von oben wird ausgelesen und in die untere Methode kopiert
-    // haben aber sonst nix miteinander zu tun die zwei p sind völlig verschieden
+    // haben aber sonst nichts miteinander zu tun die zwei p sind völlig verschieden
 
     // printPoint Methode
     static void printPoint(java.awt.Point p){
         System.out.println(p.x);    // gibt exact die daten von p aus, die aus der main Methode koordinate p.x = 10 als variable zwischengespeichert wurden
         System.out.println(p.y);    // gibt exact die daten von p aus, die aus der main Methode koordinate p.y = 20 als variable zwischengespeichert wurden
+    }
+
+    // randomPoint Methode | generiert zufallszahlen für die koordinaten x y
+    static java.awt.Point randomPoint(){
+        java.awt.Point point = new java.awt.Point();
+        point.setLocation(Math.random() * 10, Math.random() * 20);
+        return point;
+    }
+
+    // clearPoint2 Methode | löscht die koordinaten daten aus den variablen p.x & p.y = 0
+    static java.awt.Point clearPoint2(java.awt.Point p){
+        p.x = p.y = 0;
+        return p;
     }
 }
