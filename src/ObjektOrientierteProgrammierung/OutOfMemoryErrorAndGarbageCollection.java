4a712457224b662ed49cd001ef294e88d4e53e59
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-23 09:49:10 +0200

    update

---
+++ src/ObjektOrientierteProgrammierung/OutOfMemoryErrorAndGarbageCollection.java
@@ -0,0 +1,14 @@
+package ObjektOrientierteProgrammierung;
+
+/**
+ * Lernziel:
+ * - Heap
+ * - OutOfMemoryError
+ * - Garbage Collector
+ * alle Objekte die angelegt werden, leben auf dem Heap, der jedoch hat eine beschränkte größe
+ */
+public class OutOfMemoryErrorAndGarbageCollection {
+    public static void main(String[] args) {
+
+    }
+}
