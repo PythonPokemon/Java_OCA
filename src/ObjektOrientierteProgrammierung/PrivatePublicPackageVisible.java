18f861771551d8f1843df0ede01118ec5ac0b6d9
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-25 16:09:33 +0200

    update

--- src/ObjektOrientierteProgrammierung/PrivatePublicPackageVisible.java
+++ src/ObjektOrientierteProgrammierung/PrivatePublicPackageVisible.java
@@ -3,6 +3,9 @@ package ObjektOrientierteProgrammierung;
  * Lernziel:
  * - public, private
  * - Paketsichtbarkeit
+ * Zusammenfassung nur weil Klassen öffentlich sind, bedeutet das nicht dass die Methoden auch öffentlich zugänglich sind.
+ * Methoden müssen auch über einen zugrifsmodifizierer entsprechend (public, protected, private) gesetzt werden.
+ * wenn man auf sie zugreifen möchte.
  * */
 
 import de.Verweise.quote.Quote;
