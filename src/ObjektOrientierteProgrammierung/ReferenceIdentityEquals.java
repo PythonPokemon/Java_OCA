25a7a323bc4f9425fb8e1ee0cd7a24a07160b497
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-25 11:13:48 +0200

    update

--- src/ObjektOrientierteProgrammierung/ReferenceIdentityEquals.java
+++ src/ObjektOrientierteProgrammierung/ReferenceIdentityEquals.java
@@ -10,7 +10,27 @@ public class ReferenceIdentityEquals {
 
         java.awt.Point p = new java.awt.Point();
         java.awt.Point q = new java.awt.Point();
-        java.awt.Point r = new java.awt.Point();
+        java.awt.Point r = p;
 
+        System.out.println(p);
+        System.out.println(q);
+        System.out.println(r);
+
+        System.out.println( p == q);    // false
+        System.out.println(p == r);     // true | nur, weil oben r = p entspricht, | man sagt, wenn beide den gleichen Zustand haben, sind beide gleichwertig
+        System.out.println(q == r);     // false
+
+        // Gleichwertigkeit | .equals sind fragen!
+        System.out.println(p.equals( q ));  // hier wird gefragt, ob laut dieser deklaration / initialisierung p gleichwertig q ist, wenn ja, wird das ergebnis im boolean wert aus der konsole ausgegeben
+        System.out.println(q.equals( p ));
+
+        System.out.println(p.equals("abcdefg"));    // false da beide nicht gleichwertig vom datentyp sind
+        System.out.println("abcdefg".equals(p));    // false
+
+        // ACHTUNG | es ist nicht möglich, auf einer Referenz variable, die null ist, eine Methode aufzurufen!
+        // = Das wird grundsätzlich immer zur NullPointerException führen.
+        System.out.println(p.equals( null ));   // Die Frage würde einfach nur zu False führen!
+        p = null;                               //  aber das zum Error!
+        System.out.println(p.equals( null ));   // Error. .NullPointerException
     }
 }
