8eec2a42be0b9ed72491da51f214e37a6e6faf81
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-25 14:00:18 +0200

    update

--- src/ObjektOrientierteProgrammierung/MyPackageDeclaration.java
+++ src/ObjektOrientierteProgrammierung/MyPackageDeclaration.java
@@ -1,5 +1,11 @@
-package ObjektOrientierteProgrammierung;
+package ObjektOrientierteProgrammierung;    // das sagt aus das die datei: MyPackageDeclaration im packet:
 
+/*
+* Lernziel:
+* - eigene Pakete deklarieren
+* - Zusammenhang Paket und Verzeichnis
+* - man kann eine klasse per drag and drop aus dem verzeichnis in ein packet einsetzten
+* */
 public class MyPackageDeclaration {
     public static void main(String[] args) {
 
