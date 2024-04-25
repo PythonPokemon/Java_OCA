5744d6181bf0910987da7eb1ba7408a340c03c2f
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-25 16:15:35 +0200

    update

--- src/ObjektOrientierteProgrammierung/PrivatePublicPackageVisible.java
+++ src/ObjektOrientierteProgrammierung/PrivatePublicPackageVisible.java
@@ -6,6 +6,11 @@ package ObjektOrientierteProgrammierung;
  * Zusammenfassung nur weil Klassen öffentlich sind, bedeutet das nicht dass die Methoden auch öffentlich zugänglich sind.
  * Methoden müssen auch über einen zugrifsmodifizierer entsprechend (public, protected, private) gesetzt werden.
  * wenn man auf sie zugreifen möchte.
+ * Bsp. Adresse, Haus und zimmer.manche sind öffentlich zugänglich, manche sind privat
+ * Best Practice = die sichtbarkeit sollte so eingeschränkt sein wie möglich = hacking sonst können datentypen parameter verändert werden!
+ *
+ * Was nicht geht!
+ * man kann die sichtbarkeit nicht einschränken auf = nur sichtbar für klassen a, b, c!
  * */
 
 import de.Verweise.quote.Quote;
