71b5978d02a1e5a1f1657cfe3fd851fcfe8d220a
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-25 15:06:34 +0200

    update

---
+++ src/ObjektOrientierteProgrammierung/PrivatePublicPackageVisible.java
@@ -0,0 +1,12 @@
+package ObjektOrientierteProgrammierung;
+/*
+* Lernziel:
+* - public, private
+* - Paketsichtbarkeit
+* */
+
+public class PrivatePublicPackageVisible {
+    public static void main(String[] args) {
+
+    }
+}
