92338d71ab996695865f4f8f9349a6ca4f6ebcf7
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-26 09:13:07 +0200

    update

--- src/TestAufgaben/LZK4.java
+++ src/TestAufgaben/LZK4.java
@@ -1,5 +1,11 @@
 package TestAufgaben;
-
+/* Code Beschreibung.
+* Das Programm überprüft, ob der Wert "Tom" im Array "cartoons" vorkommt, und inkrementiert den Zähler "counter", wenn "Tom" gefunden wird.
+* Es prüft nacheinander die Elemente des Arrays und inkrementiert den Zähler nur einmal, selbst wenn "Tom" mehrmals vorkommt.
+* Da "Tom" am Anfang des Arrays gefunden wird, wird der Zähler auf 1 erhöht.
+* Danach werden keine weiteren Überprüfungen durchgeführt, da die Bedingungen der if-Abfragen sequenziell sind und nach der ersten Übereinstimmung keine weiteren Überprüfungen stattfinden.
+* Deshalb wird die Ausgabe des Programms "1" sein.
+* */
 public class LZK4 {
     public static void main(String[] args) {
         String[] cartoons = {"Tom", "Jerry", "Micky", "Tom"};
