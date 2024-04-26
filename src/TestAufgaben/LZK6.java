e76efb7cfa7e8843fb71ce6910ff12df433771a9
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-26 09:25:50 +0200

    update

--- src/TestAufgaben/LZK6.java
+++ src/TestAufgaben/LZK6.java
@@ -1,5 +1,16 @@
 package TestAufgaben;
 
+/* Code Beschreibung.
+ * Zuerst wird überprüft, ob "value" größer oder gleich 0 ist.
+ * Da "value" 33 ist und größer als 0, wird der Block innerhalb dieser Bedingung ausgeführt.
+ * Innerhalb dieser Bedingung gibt es weitere Bedingungen:
+ * Die zweite Bedingung überprüft, ob "value" ungleich 0 ist. Da "value" 33 ist, wird "The " ausgegeben.
+ * Die dritte Bedingung wird übersprungen, da "value" größer als 10 ist.
+ * Die vierte Bedingung überprüft, ob "value" größer als 30 ist. Da "value" 33 ist, wird "fox " ausgegeben.
+ * Die fünfte Bedingung wird übersprungen, da "value" größer als 10 ist.
+ * Die sechste Bedingung überprüft, ob "value" größer als 10 ist. Da "value" 33 ist, wird "lazy " ausgegeben.
+ * Schließlich wird "The fox lazy" ausgegeben.
+ * */
 public class LZK6 {
     public static void main(String[] args) {
         int value = 33;
