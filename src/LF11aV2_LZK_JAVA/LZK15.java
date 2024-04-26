47134ba371f3a1bf2d0b8caf34dd7b87115cbff4
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-26 14:34:48 +0200

    update

---
+++ src/LF11aV2_LZK_JAVA/LZK15.java
@@ -0,0 +1,10 @@
+package LF11aV2_LZK_JAVA;
+
+public class LZK15 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            System.out.println("i = " + i);
+            i = i + 1;
+        }
+    }
+}
---
+++ src/LF11aV2_LZK_JAVA/LZK16.java
@@ -0,0 +1,11 @@
+package LF11aV2_LZK_JAVA;
+
+public class LZK16 {
+    public static void main(String[] args) {
+        int[] x = {6, 7, 8};
+        for (int i : x){
+            System.out.println(i + " ");
+            i++;
+        }
+    }
+}
