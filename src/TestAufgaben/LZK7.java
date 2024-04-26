b5862837a4007438bc35f9d08e2fedf41085fc57
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-26 09:30:02 +0200

    update

--- src/TestAufgaben/LZK7.java
+++ src/TestAufgaben/LZK7.java
@@ -1,10 +1,15 @@
 package TestAufgaben;
-/*
-* In diesem Code überprüfen wir mit der Bedingung if (number % 2 == 0) für jede Zahl in num, ob sie durch 2 teilbar ist
-* (was bedeutet, dass sie gerade ist).
-* Wenn die Bedingung erfüllt ist, wird die Zahl zur Summe sum hinzugefügt.
-* Am Ende wird die Summe der geraden Zahlen ausgegeben.
-* */
+
+/* Code Beschreibung.
+ * Dieses Java-Programm berechnet die Summe aller geraden Zahlen in einem Array von ganzen Zahlen.
+ * Ein Array namens "num" wird erstellt und mit den Werten {10, 15, 2, 17} initialisiert.
+ * Eine Variable "sum" wird initialisiert, um die Summe der geraden Zahlen zu halten.
+ * Eine for-each-Schleife wird verwendet, um jedes Element des Arrays "num" zu durchlaufen.
+ * Innerhalb der Schleife wird überprüft, ob die aktuelle Zahl durch 2 teilbar ist (dh, ob sie gerade ist), indem der Modulo-Operator (%) verwendet wird.
+ * Wenn die Zahl gerade ist, wird sie zur Summe hinzugefügt.
+ * Schließlich wird die Summe der geraden Zahlen mit "System.out.println" ausgegeben.
+ * Die Ausgabe dieses Programms wird sein: 10 + 2 = 12
+ * */
 public class LZK7 {
     public static void main(String[] args) {
         int num[] = {10, 15, 2, 17};
