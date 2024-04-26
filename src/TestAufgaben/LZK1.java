3cbcdcdb0e93fcfc5992c6fb93a0c645ac05fa98
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-26 09:02:30 +0200

    update

--- src/TestAufgaben/LZK1.java
+++ src/TestAufgaben/LZK1.java
@@ -1,5 +1,16 @@
 package TestAufgaben;
-
+/* Beschreibung.
+* Es enthält zwei verschachtelte for-Schleifen.
+* Die äußere Schleife wird dreimal durchlaufen, da der Zähler "i" von 0 bis 2 geht (0, 1, 2).
+* Bei jedem Durchlauf wird der Zähler "count" auf 0 zurückgesetzt.
+* Die innere Schleife wird ebenfalls drei Mal durchlaufen, da der Zähler "j" von 3 bis 1 heruntergezählt wird.
+* In jedem Durchlauf der inneren Schleife wird überprüft, ob "i" gleich "j" ist.
+* Wenn dies der Fall ist, wird die Anweisung "break" ausgeführt, was bedeutet, dass die innere Schleife vorzeitig beendet wird.
+* Dabei wird auch der Zähler "count" um 1 erhöht.
+* In jedem Durchlauf der inneren Schleife wird "count" mit "System.out.print(count)" ausgegeben.
+* Allerdings wird die Anweisung "continue" verwendet, was bedeutet, dass der Rest des Schleifenkörpers übersprungen wird und die Schleife direkt zum nächsten Durchlauf springt.
+* Die Ausgabe dieses Programms wird also sein: "000". Das liegt daran, dass die äußere Schleife drei Mal durchlaufen wird und bei jedem Durchlauf der inneren Schleife "count" (der auf 0 gesetzt ist) dreimal ausgegeben wird, ohne dass sich "count" ändert, da die innere Schleife durch "continue" vorzeitig beendet wird.
+* */
 public class LZK1 {
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
@@ -9,7 +20,7 @@ public class LZK1 {
                     count++;
                     break;
                 }
-                System.out.print(count);
+                System.out.print(count); // durlauf i 000 und j 000 = 000000
                 continue;
             }
         }
--- src/TestAufgaben/LZK2.java
+++ src/TestAufgaben/LZK2.java
@@ -3,11 +3,10 @@ package TestAufgaben;
 public class LZK2 {
 
     private int i;
-
     void disp() {
         while (i <= 5) {
-            for (int i = 1; i <= 5;) {
-                System.out.print(i + " ");
+            for (int i = 1; i <= 5; ) {
+                System.out.print(i + " ");  //  Printed 6x 1 2 3 4 5 | 1 2 3 4 5 | 1 2 3 4 5 | 1 2 3 4 5 | 1 2 3 4 5 | 1 2 3 4 5
                 i++;
             }
             i++;
