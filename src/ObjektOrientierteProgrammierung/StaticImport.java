6a3f18cd1aedaa1ef9cd61c55f82d20359ab98af
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-26 15:57:07 +0200

    update

---
+++ src/ObjektOrientierteProgrammierung/StaticImport.java
@@ -0,0 +1,15 @@
+package ObjektOrientierteProgrammierung;
+import java.awt.*;
+
+/*
+* Lernziel:
+* - Statische Imports deklartion
+* */
+public class StaticImport {
+    public static void main(String[] args) {
+
+//        java.awt.Point p; // wird zu Point p; wenn mn import benutzt, um code zu sparen
+        Point p;
+
+    }
+}
