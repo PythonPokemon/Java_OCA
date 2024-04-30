ad136720b847e1e6f9977204769e9132f4c7a723
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-30 11:11:50 +0200

    update

--- src/Arrays/FirstArray.java
+++ src/Arrays/FirstArray.java
@@ -21,7 +21,7 @@ public class FirstArray {
         int[] primes = new int[5];
         System.out.println(primes.length);  // .length ist ein public attribut, es ist final, man kann es nicht modifizieren
         // primes.length = 10; geht nicht!
-        primes = new int[10];               // geht! | da der Garbage collector die neu erzeugte variable über die alte überschreibt!
+//        primes = new int[10];             // geht! | da der Garbage collector die neu erzeugte variable über die alte überschreibt!
         System.out.println(primes[0]);      // arrays starten bei 0
         System.out.println(primes[primes.length - 1]);
 
@@ -29,9 +29,11 @@ public class FirstArray {
         primes[1] = 3;
         primes[2] = 5;
         primes[3] = 7;
-        primes[4] = 11;
+        primes[4] = primes[3] + 3;  // bedeutet 7 + 3 = 10
+        primes[4]++;                // bedeutet 7 + 3 = 10 | addition plus 1 = 11
 
-        System.out.println(primes[primes.length / 2]);
+        System.out.println(primes[primes.length / 2]);  // gibt das prime, aus der mitte des index aus
+        System.out.println(primes[primes.length - 1]);  // gibt den wert des letzten array aus der konsole wieder!
 
         /*
         *             +---+---+---+---+----+
