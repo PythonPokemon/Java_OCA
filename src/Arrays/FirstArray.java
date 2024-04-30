64e4fece63fc7a2e649b1b2b510fe7491f0bdde2
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-30 09:57:42 +0200

    update

---
+++ src/Arrays/FirstArray.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+/*
+*   Lernziel:
+*   - Array-Objekte aufbauen
+*   - Referenzen
+*   - Initialisierung bei primitiven Elementen und Referenzen
+*   - length Attribut
+*   - Elementzugriff, lesen und schreiben
+*   - IndexOutBoundsException
+*   - Arrays an Methoden übergeben
+* */
+public class FirstArray {
+}
