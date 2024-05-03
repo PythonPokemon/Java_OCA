0efdf05625817edd47eb787dc5de1812430193a2
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-03 16:43:38 +0200

    update

---
+++ src/JACA_OCA_Test_1/Aufgaben_10_bis_19/test15.java
@@ -0,0 +1,18 @@
+package JACA_OCA_Test_1.Aufgaben_10_bis_19;
+
+public class test15 {
+    public static void main(String[] args) {
+        System.out.println("_____ uses access modifiers to protect variables and hide them within a class.");
+        System.out.println("Which of the following options accurately fill in the blanks above?");
+
+        boolean Encapsulation = true;   // korrekt answer!
+        boolean Inheritance = false;
+        boolean Abstraction = false;
+        boolean Polymorphism = false;
+
+        System.out.println(Encapsulation);
+        System.out.println(Inheritance);
+        System.out.println(Abstraction);
+        System.out.println(Polymorphism);
+    }
+}
