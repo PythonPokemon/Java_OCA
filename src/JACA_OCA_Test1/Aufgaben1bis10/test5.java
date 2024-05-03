bdb8065dee85e2ba876135ecdf6df266e41438cd
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-03 09:50:03 +0200

    update

---
+++ src/JACA_OCA_Test1/Aufgaben1bis10/test5.java
@@ -0,0 +1,14 @@
+package JACA_OCA_Test1.Aufgaben1bis10;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class test5 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2012, 1, 11);
+        Period period = Period.ofMonths(2);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yy");
+        System.out.println(formatter.format(date.minus(period)));   // 11-11-11
+    }
+}
