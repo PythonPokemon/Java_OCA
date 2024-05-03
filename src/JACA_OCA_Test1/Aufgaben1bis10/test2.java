a7c2e37f98d7d26c0b45a1f5dbb928db179ad2a8
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-03 09:26:27 +0200

    update

---
+++ src/JACA_OCA_Test1/Aufgaben1bis10/test2.java
@@ -0,0 +1,19 @@
+package JACA_OCA_Test1.Aufgaben1bis10;
+
+public class test2 {
+    public static void main(String[] args) {
+        int [] arr3 [] = new int[8][];
+        System.out.println(arr3);
+
+        int [] arr1 = new int[8];
+        System.out.println(arr1);
+
+        int [][] arr2 = new int[8][8];
+        System.out.println(arr2);
+
+
+        // falsche deklaration
+//        int arr4 [][] = new int[][8]; // error
+//        System.out.println(arr4);     // error
+    }
+}
