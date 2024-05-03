67c0df74d898d19451931a35b6c8f36587f67169
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-03 19:26:06 +0200

    update

---
+++ src/JACA_OCA_Test_1/Aufgaben_20_bis_29/test20.java
@@ -0,0 +1,24 @@
+package JACA_OCA_Test_1.Aufgaben_20_bis_29;
+
+public class test20 {
+
+    // compiler fehler | zum Testen auskommentieren!
+//    public static void main(String[] args) {
+//        int grade = 60;
+//        if(grade = 60)  // <-- da hier ein zuweisungsoperator = | anstatt vergleichs operator == verwendet wurde!
+//            System.out.println("You passed...");
+//        else
+//            System.out.println("You failed");
+//    }
+
+
+    // Korrekt wäre
+    public static void main(String[] args) {
+        int grade = 60;
+        if(grade == 60)  // <-- vergleichs operator == verwenden!
+            System.out.println("You passed...");
+        else
+            System.out.println("You failed");
+
+    }
+}
---
+++ src/JACA_OCA_Test_1/Aufgaben_20_bis_29/test21.java
@@ -0,0 +1,35 @@
+/*
+* Der Compiler fehler tritt aufgrund der Unvollständigkeit des ternären Operators (?:) in deinem Code auf.
+* In Java muss der ternäre Operator immer drei Operanden haben: ein Bedingungsausdruck gefolgt von einem Ausdruck,
+* der ausgewertet wird, wenn die Bedingung wahr ist, und einem Ausdruck, der ausgewertet wird, wenn die Bedingung falsch ist.
+
+In deinem Code hast du zwei Bedingungsausdrücke hintereinander ohne einen Ausdruck, der ausgewertet wird, wenn die Bedingung wahr ist. Das führt zu einem Compilerfehler.
+*
+In Java ist das Fragezeichen ? ein ternärer Operator, der als bedingter Operator bekannt ist.
+* Der ternäre Operator hat die folgende Syntax: bedingung ? ausdruck1 : ausdruck2
+*
+Das Doppelpunkt-Zeichen (:) in Java wird verwendet, um zwischen den beiden möglichen Ausdrücken zu trennen, die im ternären Operator (? :) verwendet werden.
+* Es fungiert als Trennzeichen zwischen dem Ausdruck, der ausgewertet wird, wenn die Bedingung wahr ist, und dem Ausdruck, der ausgewertet wird, wenn die Bedingung falsch ist.
+* */
+
+package JACA_OCA_Test_1.Aufgaben_20_bis_29;
+import  java.time.LocalTime;
+
+
+// Compiler fehler | zum Testen auskommentieren
+//public class test21 {
+//    public static void main(String[] args) {
+//    LocalTime time = LocalTime.of(16, 40);
+//    String amPm = time.getHour() >= 12 ? (time.getHour() == 12) ? "PM" : "AM";    // <-- fehler
+//        System.out.println(amPm);
+//    }
+//}
+
+// Korrekt wäre das!
+public class test21 {
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.of(16, 40);
+        String amPm = time.getHour() >= 12 ? (time.getHour() == 12 ? "PM" : "PM") : "AM";   // <--
+        System.out.println(amPm);
+    }
+}
