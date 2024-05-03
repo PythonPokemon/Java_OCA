c05eec3fbed8760f80a555381c8f081963041880
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-03 09:08:13 +0200

    update

---
+++ src/JACA_OCA_Test1/Aufgaben1bis10/test1.java
@@ -0,0 +1,10 @@
+package JACA_OCA_Test1.Aufgaben1bis10;
+
+public class test1 {
+    public static void main(String[] args) {
+        do {
+            System.out.println(100);
+        } while (false);
+        System.out.println("Bye");
+    }
+}
