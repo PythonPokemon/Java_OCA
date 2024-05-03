9519eea9ad65ee5ae8c54a4eaee148f3325abfbc
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-03 11:28:57 +0200

    update

--- src/JACA_OCA_Test1/Aufgaben1bis10/test8.java
+++ src/JACA_OCA_Test1/Aufgaben1bis10/test8.java
@@ -1,4 +1,25 @@
 package JACA_OCA_Test1.Aufgaben1bis10;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+* Die main()-Methode, die den Einstiegspunkt für die Ausführung eines Java-Programms darstellt, muss immer öffentlich sein und genau die Signatur public static void main(String[] args) haben.
+* Dies liegt daran, dass der Java-Laufzeitumgebung (java-Befehl) diese Methode benötigt, um das Programm zu starten.
+* Das bedeutet, dass unabhängig von der Sichtbarkeit (public, private oder protected) der Klasse, die main()-Methode öffentlich sein muss.
+* Wenn du versuchst, die main()-Methode als private oder protected zu deklarieren, wird dies zu einem Compilerfehler führen und das Programm wird nicht kompilieren.
+* */
+
 public class test8 {
+    //  Variante 1 | auskommentieren zum testen
+//    public static void main(String[] args) {
+//        System.out.println("Hurrah! I passed...");
+//    }
+
+    // Variante 2
+    static public void main(String[] args) {
+        System.out.println("Hurrah! I passed...");
+    }
 }
---
+++ src/JACA_OCA_Test1/Aufgaben1bis10/test9.java
@@ -0,0 +1,28 @@
+package JACA_OCA_Test1.Aufgaben1bis10;
+
+
+class Point {
+    int x;
+    int y;
+
+    void assign(int x, int y) {
+        x = this.x;
+        this.y = y;
+    }
+
+    public String tostring() {
+        return "Point(" + x + ", " + y + ")";
+    }
+}
+
+public class test9 {
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        p1.x = 10;
+        p1.y = 20;
+        Point p2 = new Point();
+        p2.assign(p1.x, p1.y);
+        System.out.println(p1.tostring() + ":" + p2.tostring());    // Point(10, 20):Point(0, 20)
+    }
+}
+
