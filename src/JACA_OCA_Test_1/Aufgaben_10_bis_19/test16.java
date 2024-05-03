c247eabc4d9d3fa4ebef9b1d7b2ab8772dfcf122
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-03 17:01:29 +0200

    update

--- src/JACA_OCA_Test_1/Aufgaben_10_bis_19/test16.java
+++ src/JACA_OCA_Test_1/Aufgaben_10_bis_19/test16.java
@@ -1,10 +1,10 @@
 package JACA_OCA_Test_1.Aufgaben_10_bis_19;
 /*
-* In diesem Code tritt eine Ausnahme auf, wenn die statische Initialisierung des Klassenblocks durchgeführt wird.
-* Der Code im statischen Initialisierungsblock wird ausgeführt, bevor die main()-Methode aufgerufen wird.
-* Wenn eine Ausnahme während der Ausführung des statischen Initialisierungsblocks auftritt, wird der Code darunter nicht mehr erreicht.
-* In dem Fall führt der Ausdruck System.out.println(1/0); im statischen Initialisierungsblock zu einer Division durch Null, was eine ArithmeticException verursacht.
-* Da eine Ausnahme im statischen Initialisierungsblock auftritt, wird die main()-Methode nicht erreicht und daher wird "HELLO" nicht in der Konsole ausgegeben.*/
+ * In diesem Code tritt eine Ausnahme auf, wenn die statische Initialisierung des Klassenblocks durchgeführt wird.
+ * Der Code im statischen Initialisierungsblock wird ausgeführt, bevor die main()-Methode aufgerufen wird.
+ * Wenn eine Ausnahme während der Ausführung des statischen Initialisierungsblocks auftritt, wird der Code darunter nicht mehr erreicht.
+ * In dem Fall führt der Ausdruck System.out.println(1/0); im statischen Initialisierungsblock zu einer Division durch Null, was eine ArithmeticException verursacht.
+ * Da eine Ausnahme im statischen Initialisierungsblock auftritt, wird die main()-Methode nicht erreicht und daher wird "HELLO" nicht in der Konsole ausgegeben.*/
 
 
 //------ArithmeticException | zum Testen auskommentieren!
@@ -20,12 +20,12 @@ package JACA_OCA_Test_1.Aufgaben_10_bis_19;
 //}
 
 /*----------------------------------------------------------------------------------------------------------------------
-* Um "HELLO" in der Konsole auszugeben, musst du sicherstellen, dass der Code im statischen Initialisierungsblock erfolgreich ausgeführt wird.
-* Das bedeutet, dass du sicherstellen musst, dass keine Ausnahmen auftreten.
-* In diesem Fall könntest du z.B. die Division durch Null im statischen Initialisierungsblock vermeiden.
-*
-* Hier ist ein Beispiel:
-* */
+ * Um "HELLO" in der Konsole auszugeben, musst du sicherstellen, dass der Code im statischen Initialisierungsblock erfolgreich ausgeführt wird.
+ * Das bedeutet, dass du sicherstellen musst, dass keine Ausnahmen auftreten.
+ * In diesem Fall könntest du z.B. die Division durch Null im statischen Initialisierungsblock vermeiden.
+ *
+ * Hier ist ein Beispiel:
+ * */
 
 // Korrekt wäre das!
 public class test16 {
