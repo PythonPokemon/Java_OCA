d89ed99dbf2d4694dec3f8229fbcd46261e1ea5f
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-06 15:18:45 +0200

    update

--- src/JACA_OCA_Test_1/Aufgaben_20_bis_29/test29.java
+++ src/JACA_OCA_Test_1/Aufgaben_20_bis_29/test29.java
@@ -1,21 +1,27 @@
 /*
-* In diesem Code wird eine Klasse mit dem Namen test29 definiert. Innerhalb dieser Klasse werden drei Instanzvariablen deklariert, aber nicht initialisiert:
+* In diesem Code wird eine Klasse mit dem Namen test29 definiert.
+* Innerhalb dieser Klasse werden drei Instanzvariablen deklariert, aber nicht initialisiert:
 *
 * var1 vom Typ char.
 * var2 vom Typ double.
 * var3 vom Typ float.
 *
-* In Java werden Instanzvariablen automatisch mit einem Standardwert initialisiert, wenn sie nicht explizit initialisiert werden. Die Standardinitialisierung erfolgt wie folgt:
+* In Java werden Instanzvariablen automatisch mit einem Standardwert initialisiert, wenn sie nicht explizit initialisiert werden.
+* Die Standardinitialisierung erfolgt wie folgt:
 *
 * Für numerische Datentypen (wie char, double, float, etc.) ist der Standardwert 0. | <---- WICHTIG
 * Für Objektreferenzen (wie String, ArrayList, usw.) ist der Standardwert null.     | <---- WICHTIG
 *
-* Daher werden in der main-Methode des test29-Objekts erstellt (obj), aber da keine expliziten Werte für die Instanzvariablen zugewiesen wurden, behalten sie ihre Standardwerte bei:
+* Daher werden in der main-Methode des test29-Objekts erstellt (obj),
+* aber da keine expliziten Werte für die Instanzvariablen zugewiesen wurden, behalten sie ihre Standardwerte bei:
 *
 * var1 wird als '\u0000' initialisiert, was das Unicode-Nullzeichen darstellt.
 * var2 wird als 0.0 initialisiert.
 * var3 wird als 0.0f initialisiert.
-* Der Code gibt dann die Werte dieser Instanzvariablen aus. Da sie nicht explizit initialisiert wurden, werden die Standardwerte für diese Datentypen angezeigt:*/
+*
+* Der Code gibt dann die Werte dieser Instanzvariablen aus.
+* Da sie nicht explizit initialisiert wurden, werden die Standardwerte für diese Datentypen angezeigt:
+* */
 
 package JACA_OCA_Test_1.Aufgaben_20_bis_29;
 
