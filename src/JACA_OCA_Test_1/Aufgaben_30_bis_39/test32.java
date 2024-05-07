420e8a98fe1b961da503f201feddea002a11b7ba
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-07 09:14:02 +0200

    update

---
+++ src/JACA_OCA_Test_1/Aufgaben_30_bis_39/test32.java
@@ -0,0 +1,13 @@
+package JACA_OCA_Test_1.Aufgaben_30_bis_39;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test32 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+
+        System.out.println(list);
+    }
+}
