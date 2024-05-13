40c31bc01b2ea52f52e531f1076ef71776ba95ca
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-13 15:55:01 +0200

    update

--- src/JACA_OCA_Test_1/Aufgaben_60_bis_70/test65.java
+++ src/JACA_OCA_Test_1/Aufgaben_60_bis_70/test65.java
@@ -1,3 +1,11 @@
+/*
+* Der Code erstellt eine Liste von Ganzzahlen (Integer) und fügt die Werte 2, 1 und 0 der Liste hinzu.
+* Dann wird der Index des Werts 0 in der Liste gefunden und dieser Wert wird entfernt.
+* Da indexOf den Index des ersten Auftretens des angegebenen Elements in der Liste zurückgibt,
+* wird in diesem Fall der Index des Werts 0 gefunden und entfernt.
+*
+* Die Liste enthält nach dem Entfernen des Werts 0 die verbleibenden Werte 2 und 1.
+* */
 package JACA_OCA_Test_1.Aufgaben_60_bis_70;
 
 import java.util.ArrayList;
