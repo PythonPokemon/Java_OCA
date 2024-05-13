58c51cd4413cf41adb29511977e841d3991e863c
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-13 15:42:00 +0200

    update

---
+++ src/JACA_OCA_Test_1/Aufgaben_60_bis_70/test63.java
@@ -0,0 +1,28 @@
+/*
+*
+In diesem Code gibt es zwei Überladungsmethoden mit dem Namen m, eine für den int-Typ und eine für den char-Typ.
+
+Wenn Sie m(i) aufrufen, wird die Methode m(int x) aufgerufen, da i vom Typ int ist. Der ASCII-Wert des Zeichens '5' ist 53, daher wird m(i) "int version" ausgeben.
+
+Wenn Sie m('5') aufrufen, wird die Methode m(char x) aufgerufen, da der Wert '5' vom Typ char ist. Daher wird m('5') "char version" ausgeben.
+
+Obwohl das Zeichen '5' als Zeichen konstant dargestellt ist, kann es in einem numerischen Kontext als Zahl verwendet werden. In diesem Fall wird es als ASCII-Wert behandelt, der dem numerischen Wert 53 entspricht.
+* */
+
+package JACA_OCA_Test_1.Aufgaben_60_bis_70;
+
+public class test63 {
+    private static void m(int x) {
+        System.out.println("int version");
+    }
+
+    private static void m(char x) {
+        System.out.println("char version");
+    }
+
+    public static void main(String[] args) {
+        int i = '5';
+        m(i);
+        m('5');
+    }
+}
---
+++ src/JACA_OCA_Test_1/Aufgaben_60_bis_70/test64B.java
@@ -0,0 +1,7 @@
+package JACA_OCA_Test_1.Aufgaben_60_bis_70;
+
+public class test64B extends test64A{
+    public void print() {
+        System.out.println("test64A");
+    }
+}
