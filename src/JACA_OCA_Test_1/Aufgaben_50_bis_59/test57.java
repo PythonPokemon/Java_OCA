c2eddb4f09581212409196455491f1436fde563c
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-13 14:29:08 +0200

    update

--- src/JACA_OCA_Test_1/Aufgaben_50_bis_59/test57.java
+++
@@ -1,7 +0,0 @@
-package JACA_OCA_Test_1.Aufgaben_50_bis_59;
-
-public class test57 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/JACA_OCA_Test_1/Aufgaben_50_bis_59/test57A.java
@@ -0,0 +1,6 @@
+package JACA_OCA_Test_1.Aufgaben_50_bis_59;
+
+public class test57A {
+    public int i1 = 1;
+    protected int i2 = 2;
+}
