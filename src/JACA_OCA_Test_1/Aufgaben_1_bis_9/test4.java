a29c7219d5a2e871f2a56493cd3b09ecddc35904
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-14 09:53:41 +0200

    update

--- src/JACA_OCA_Test_1/Aufgaben_1_bis_9/test4.java
+++ src/JACA_OCA_Test_1/Aufgaben_1_bis_9/test4.java
@@ -1,3 +1,26 @@
+/*
+* Der Code enthält eine abstrakte Klasse Animal und eine konkrete Klasse Dog, die von Animal erbt. Hier ist eine Erklärung:
+
+Was passiert im Code?
+Abstrakte Klasse Animal:
+
+Definiert eine abstrakte Klasse Animal, die einen privaten Namen (name) enthält.
+Der Konstruktor Animal(String name) setzt den Namen des Tieres.
+Die Methode getName() gibt den Namen des Tiers zurück.
+Klasse Dog (Hund):
+
+Erbt von der Klasse Animal.
+Enthält ein zusätzliches privates Attribut breed (Rasse) spezifisch für Hunde.
+Der erste Konstruktor Dog(String breed) setzt die Rasse des Hundes, aber keinen Namen.
+Der zweite Konstruktor Dog(String name, String breed) setzt sowohl den Namen als auch die Rasse des Hundes.
+Die Methode getBreed() gibt die Rasse des Hundes zurück.
+Klasse test4:
+
+Die main-Methode erstellt zwei Hunde-Objekte (dog1 und dog2) mit verschiedenen Konstruktoren.
+dog1 hat nur die Rasse "Beagle", dog2 hat den Namen "Bubbly" und die Rasse "Pudel".
+Es wird versucht, die Namen und Rassen der Hunde auf der Konsole auszugeben.
+* */
+
 //package JACA_OCA_Test1.Aufgaben1bis10;
 //-------Auskommentieren zum Testen--------------
 //// Was kommt raus?
