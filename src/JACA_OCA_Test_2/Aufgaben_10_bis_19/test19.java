6cfc67440d9ed35d70501c3c2abf7f6e8b4a1c05
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-15 19:01:01 +0200

    update

--- src/JACA_OCA_Test_2/Aufgaben_10_bis_19/test19.java
+++ src/JACA_OCA_Test_2/Aufgaben_10_bis_19/test19.java
@@ -20,13 +20,13 @@ Es sollte nichts zwischen if-else-Block stehen, aber in diesem Fall System.out.p
 
 package JACA_OCA_Test_2.Aufgaben_10_bis_19;
 
-public class test19 {
-    public static void main(String[] args) {
-        int grade = 75;
-        if(grade > 60)
-            System.out.println("Congratulations");
-        System.out.println("You passed");
-         else
-        System.out.println("You failed");
-    }
-}
+//public class test19 {
+//    public static void main(String[] args) {
+//        int grade = 75;
+//        if(grade > 60)
+//            System.out.println("Congratulations");
+//        System.out.println("You passed");
+//         else
+//        System.out.println("You failed");
+//    }
+//}
