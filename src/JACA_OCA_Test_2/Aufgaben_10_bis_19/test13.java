328be7cdbd5e4a7a0d47d6d883231727e22492b7
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-15 19:00:42 +0200

    update

---
+++ src/JACA_OCA_Test_2/Aufgaben_10_bis_19/test13.java
@@ -0,0 +1,40 @@
+/*
+ * Die folgende Anweisung in einem Java-Programm wurde erfolgreich kompiliert:
+
+student.report(course);
+
+Was können Sie mit Sicherheit sagen?
+* report ist der Name der Methode = richtig
+
+Student ist der Klassenname
+
+Student ist der Name der Referenzvariablen
+
+Kurs muss vom Typ String sein
+------------------------------------------------------------------------------------------------------------------------
+*
+* Gesamterklärung
+Es empfiehlt sich, das erste Zeichen des Klassennamens in Großbuchstaben zu schreiben, aber es ist nicht zwingend erforderlich.
+
+Student kann entweder Klassenname oder Referenzvariablenname sein.
+
+
+
+Die Syntax zum Aufrufen der statischen Methode lautet: Class_Name.method_name(<arguments>); ODER reference_variable_name.method_name(<argumente>);
+
+Die Syntax zum Aufrufen der Instanzmethode lautet: reference_variable_name.method_name(<arguments>);
+
+Wenn student class_name oder refernce_variable_name darstellt, ist report möglicherweise die statische Methode der Klasse.
+
+Wenn student reference_variable_name darstellt, ist report die Instanzmethode der Klasse.
+
+In beiden Fällen muss report der Methodenname sein.
+
+
+
+Die Art des Arguments kann nicht durch einen Blick auf die obige Syntax ermittelt werden.
+ * */
+package JACA_OCA_Test_2.Aufgaben_10_bis_19;
+
+public class test13 {
+}
