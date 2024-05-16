9e7abc2c37c96b8926e8509e0a761d6d85470418
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-16 11:32:20 +0200

    update

--- src/JACA_OCA_Test_2/Aufgaben_50_bis_59/test57.java
+++ src/JACA_OCA_Test_2/Aufgaben_50_bis_59/test57.java
@@ -1,7 +1,25 @@
 /*
+ * .NullPointerException
  *
+ * Gesamterklärung
+add(10.0, null); => Der Compiler kann null nicht in den primitiven Typ double konvertieren, daher ist das 2. Argument mit dem Double-Referenztyp gekennzeichnet.
+
+Um dem Methodenaufruf zu entsprechen, wird 10.0 durch automatisches Boxing und add(10.0, null) in ein Double-Objekt konvertiert. ist mit add(Double, Double) markiert; Methode.
+
+Aber zum Zeitpunkt der Ausführung ist d2 null, also System.out.println("Double version: " + (d1 + d2)); löst NullPointerException aus.
  * */
 package JACA_OCA_Test_2.Aufgaben_50_bis_59;
 
 public class test57 {
+    private static void add(double d1, double d2) {
+        System.out.println("double version: " + (d1 + d2));
+    }
+
+    private static void add(Double d1, Double d2) {
+        System.out.println("Double version: " + (d1 + d2));
+    }
+
+    public static void main(String[] args) {
+        add(10.0, null);
+    }
 }
