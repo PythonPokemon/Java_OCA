8f2a1d9997b65558405f8191729d0d75ee1ec95a
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-22 15:13:07 +0200

    update

--- src/JAVA_OCA_Test_2/Aufgaben_20_bis_29/test21.java
+++ src/JAVA_OCA_Test_2/Aufgaben_20_bis_29/test21.java
@@ -1,10 +1,23 @@
 /*
-* Was wird das Ergebnis des Kompilierens und Ausführens der Testklasse sein?
+Was wird das Ergebnis des Kompilierens und Ausführens der Testklasse sein?
+
+Gesamterklärung
+public void Test() ist eine Methode und kein Konstruktor, da der Rückgabetyp void ist.
+method kann denselben Namen wie der Klassenname haben, daher gibt es keine Probleme mit der Deklaration der Test()-Methode.
+------------------------------------------------------------------------------------------------------------------------
+Da für diese Klasse keine Konstruktoren verfügbar sind, fügt der Java-Compiler den folgenden Konstruktor hinzu.
+
+public Test() {}
+Test obj = new Test(); Ruft den Standardkonstruktor auf,
+ändert jedoch nicht den Wert der Eigenschaft name (standardmäßig wird der Eigenschaft name null zugewiesen)
+------------------------------------------------------------------------------------------------------------------------
+System.out.println(obj.name); gibt null aus.
 * */
 package JAVA_OCA_Test_2.Aufgaben_20_bis_29;
 
 public class test21 {
     public String name;
+
     public void test21() {
         name = "James";
     }
--- src/JAVA_OCA_Test_2/Aufgaben_20_bis_29/test22.java
+++ src/JAVA_OCA_Test_2/Aufgaben_20_bis_29/test22.java
@@ -1,5 +1,12 @@
 /*
 * false:false
+
+wenn etwas am gleichen datum ist, kann es nicht voher oder nachher sein sondern es ist gleichzeitig!
+
+Gesamterklärung
+isAfter und isBefore können wie folgt interpretiert werden:
+
+Kommt der 1. Januar 2018 nach dem 1. Januar 2018? Nein, falsch. Kommt der 1. Januar 2018 vor dem 1. Januar 2018? Nein, falsch.
 * */
 
 package JAVA_OCA_Test_2.Aufgaben_20_bis_29;
