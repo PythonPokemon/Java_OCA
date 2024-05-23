2fe90033c7ea94cee6980f397740eab4653dcb3a
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-23 16:25:26 +0200

    update

--- src/JAVA_OCA_Test_2/Aufgaben_40_bis_49/test40.java
+++ src/JAVA_OCA_Test_2/Aufgaben_40_bis_49/test40.java
@@ -1,19 +1,21 @@
 /*
-* Welche der folgenden Optionen kann *INSERT* so ersetzen, dass beim Ausführen der Testklasse alle Array-Elemente in der Ausgabe angezeigt werden? Wählen Sie ALLE zutreffenden aus.
-* */
+ * Welche der folgenden Optionen kann *INSERT* so ersetzen, dass beim Ausführen der Testklasse alle Array-Elemente in der Ausgabe angezeigt werden? Wählen Sie ALLE zutreffenden aus.
+ * */
 package JAVA_OCA_Test_2.Aufgaben_40_bis_49;
+
 import java.util.function.Predicate;
+
 public class test40 {
     public static void main(String[] args) {
-        String [] arr = {"A", "ab", "bab", "Aa", "bb", "baba", "aba", "Abab"};
-
+        String[] arr = {"A", "ab", "bab", "Aa", "bb", "baba", "aba", "Abab"};
+                                /*INSERT*/
+                            //     \/
         processStringArray(arr, p -> true);
     }
 
-    private static void processStringArray(String [] arr,
-                                           Predicate<String> predicate) {
-        for(String str : arr) {
-            if(predicate.test(str)) {
+    private static void processStringArray(String[] arr, Predicate<String> predicate) {
+        for (String str : arr) {
+            if (predicate.test(str)) {
                 System.out.println(str);
             }
         }
