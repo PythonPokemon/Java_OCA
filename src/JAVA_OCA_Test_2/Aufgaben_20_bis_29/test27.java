a676dcd304b0b861d5929cfcc41a7f11df345b88
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-23 11:22:04 +0200

    update

--- src/JAVA_OCA_Test_2/Aufgaben_20_bis_29/test27.java
+++ src/JAVA_OCA_Test_2/Aufgaben_20_bis_29/test27.java
@@ -7,9 +7,9 @@ package JAVA_OCA_Test_2.Aufgaben_20_bis_29;
 public class test27 {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("Java");
-        String s1 = sb.toString();
-        String s2 = "Java";
+        String s1 = sb.toString();  // methode()
+        String s2 = "Java"; // string
 
-        System.out.println(s1 == s2);
+        System.out.println(s1 == s2); // kann nicht gleich sein
     }
 }
--- src/JAVA_OCA_Test_2/Aufgaben_20_bis_29/test28.java
+++ src/JAVA_OCA_Test_2/Aufgaben_20_bis_29/test28.java
@@ -22,7 +22,9 @@ Aber case-Ausdrücke, score < 70 und score >= 70 sind boolescher Typ und daher K
 
 package JAVA_OCA_Test_2.Aufgaben_20_bis_29;
 
+// Comnpiler Fehler | bedingte Ausdrücke wie <, <=, >, >= usw. sind f. switch-case nicht erlaubt!
 //public class test28 {
+//    public static final int score = 60;
 //    public static void main(String[] args) {
 //        int score = 60;
 //        switch (score) {
@@ -32,6 +34,28 @@ package JAVA_OCA_Test_2.Aufgaben_20_bis_29;
 //                System.out.println("Failed");
 //                break;
 //            case score >= 70:
+//                System.out.println("Passed");
+//                break;
+//        }
+//    }
+//}
+//----------------------------------------------------------------------------------------------------------------------
+//public class test28 {
+//    public static final int SCORE = 70;
+//
+//    public static void main(String[] args) {
+//        int score = 60;
+//
+//        switch (score) {
+//            default:
+//                System.out.println("Not a valid score");
+//                break;
+//            case 0: // Dummy case to ensure a valid constant case, which won't be used
+//                break;
+//            case SCORE - 10:
+//                System.out.println("Failed");
+//                break;
+//            case SCORE:
 //                System.out.println("Passed");
 //                break;
 //        }
