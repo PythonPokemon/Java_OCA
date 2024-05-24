510c1e55c21276b0fcaf5c5e50685da36b08b165
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-24 12:09:44 +0200

    update

--- src/JAVA_OCA_Test_2/Aufgaben_40_bis_49/test45.java
+++ src/JAVA_OCA_Test_2/Aufgaben_40_bis_49/test45.java
@@ -12,9 +12,16 @@ package JAVA_OCA_Test_2.Aufgaben_40_bis_49;
 
 public class test45 {
     public static void main(String[] args) {
-        int [] arr = {3, 2, 1};
+        int [] arr = {3, 2, 1}; // 3 ist Index [0], 2 ist Index [1], 1ist Index [3]
         for(int i : arr) {
-            System.out.println(arr[i]);
+            // compiler error, weil versucht auf das [i] im array Index zu zugreifen, es gibt aber nur diese elemente 3, 2, 1 Index 0, 1, 2
+//            System.out.println(arr[i]);
+            System.out.println(arr);           // gibt die speicheradresse an: [I@6acbcfc0
+            System.out.println(i);             // gibt die Elemente des Datentyps int aus 3, 2, 1 untereinander
+            System.out.println(arr[0]);        // Expliziter ausdruck der elemente im array der sich im Index 0, 1, 2 befindet!
+
         }
     }
 }
+//----------------------------------------------------------------------------------------------------------------------
+
