4cd45946c7ea01d62fc68eb453a6ed5b347278f3
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-27 14:17:42 +0200

    update

--- src/JAVA_OCA_Test_2/Aufgaben_30_bis_39/test32.java
+++ src/JAVA_OCA_Test_2/Aufgaben_30_bis_39/test32.java
@@ -17,13 +17,16 @@ Wenn end größer als die Länge des StringBuilder-Objekts ist, wird StringIndex
 In diesem Fall entspricht 'sb.delete(0, 100);' also 'sb.delete(0, sb.length());' und löscht alle Zeichen aus dem StringBuilder-Objekt.
 
 Daher System.out.println(sb.length()); gibt 0 auf der Konsole aus.
+
  * */
 package JAVA_OCA_Test_2.Aufgaben_30_bis_39;
 
 public class test32 {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("Hurrah! I Passed..."); // ist 19 zeichen lang
-        sb.delete(0, 100);  // gibt 0 aus | wenn 0, 10 wäre der tatsächliche stringlänge 9 wäre, da 10 zeichen gelöscht werden
+
+        //Der Aufruf sb.delete(0, 100) versucht, alle Zeichen von Index 0 bis 99 zu löschen.
+        sb.delete(0, 100);  // gibt 0 aus | wenn 0, 10 wäre der tatsächliche ausgabe 9, da 10 zeichen gelöscht werden von 19
         System.out.println(sb.length());
     }
 }
