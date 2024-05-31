6000ee87ed0f715a760366201b23efdd9cb37ae5
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-05-31 13:40:23 +0200

    update

--- src/JAVA_OCA_Test_3/Aufgaben_1_bis_9/test8.java
+++ src/JAVA_OCA_Test_3/Aufgaben_1_bis_9/test8.java
@@ -1,23 +1,67 @@
 /*
-NullPointerException wird zur Laufzeit ausgelöst
+Schritt-für-Schritt-Analyse
+Deklaration des Arrays:
 
-Gesamterklärung
-Alle Arrayelemente werden mit ihren Standardwerten initialisiert. arr ist vom booleschen Typ (Referenztyp), daher wird arr[0] auf null initialisiert.
+static Boolean[] arr = new Boolean[1];
+Hier wird ein Array von Boolean-Objekten mit einer Länge von 1 deklariert.
+Da es sich um ein Array von Objekten handelt, werden die Elemente des Arrays automatisch auf null gesetzt.
+Das bedeutet, arr[0] ist null.
+------------------------------------------------------------------------------------------------------------------------
+Hauptmethode:
 
-Wenn der Ausdruck mit einer Variablen vom Typ Boolean funktioniert, gibt "if(arr[0])" keinen Kompilierungsfehler aus,
-aber die Java-Laufzeit extrahiert den in arr[0] gespeicherten booleschen Wert und verwendet die Methode booleanValue().
+public static void main(String[] args) {
+    if(arr[0]) {
+        System.out.println(true);
+    } else {
+        System.out.println(false);
+    }
+}
+In der main-Methode wird versucht, den Wert von arr[0] zu überprüfen.
 
-arr[0].booleanValue() bedeutet, dass die Methode booleanValue() bei einer Nullreferenz aufgerufen wird und daher NullPointerException zur Laufzeit ausgelöst wird.
- * */
+Hier passiert Folgendes:
+
+arr[0] ist null, da das Array nur deklariert wurde und keine Werte zugewiesen wurden.
+Der Ausdruck if(arr[0]) versucht, das Boolean-Objekt arr[0] in einen primitiven boolean-Wert umzuwandeln,
+um die Bedingung zu evaluieren.
+
+Warum tritt die NullPointerException auf?
+------------------------------------------------------------------------------------------------------------------------
+In Java führt der Versuch, ein null-Objekt in einen primitiven Typ zu konvertieren, zu einer NullPointerException.
+------------------------------------------------------------------------------------------------------------------------
+Im Detail passiert Folgendes:
+
+Der Ausdruck if(arr[0]) versucht, arr[0] (welches null ist) in einen boolean-Wert zu konvertieren.
+Da arr[0] null ist, kann die Autoboxing-Konvertierung (Boolean zu boolean) nicht durchgeführt werden,
+und es wird eine NullPointerException geworfen.* */
 package JAVA_OCA_Test_3.Aufgaben_1_bis_9;
 
-//public class test8 {
-//    static Boolean[] arr = new Boolean[1];
-//    public static void main(String[] args) {
-//        if(arr[0]) {
-//            System.out.println(true);
-//        } else {
-//            System.out.println(false);
-//        }
-//    }
-//}
+public class test8 {
+    static Boolean[] arr = new Boolean[1];
+    public static void main(String[] args) {
+        if(arr[0]) {                    // arr[0] ist kein Boolean wert deswegen | .NullPointerException
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
+/*
+Lösungsmöglichkeiten
+Um die NullPointerException zu vermeiden, musst du sicherstellen, dass arr[0] einen gültigen Boolean-Wert
+(entweder true oder false) enthält, bevor du darauf zugreifst. Hier sind einige Möglichkeiten, dies zu erreichen:
+
+Initialisierung des Arrays mit einem gültigen Boolean-Wert:
+
+static Boolean[] arr = new Boolean[]{Boolean.FALSE};
+Überprüfung auf null vor dem Zugriff:
+
+public static void main(String[] args) {
+    if(arr[0] != null && arr[0]) {
+        System.out.println(true);
+    } else {
+        System.out.println(false);
+    }
+}
+Durch eine dieser Änderungen wird sichergestellt, dass der Code nicht versucht, ein null-Objekt
+in einen primitiven boolean-Wert zu konvertieren, und somit eine NullPointerException vermieden wird.
+* */
--- src/JAVA_OCA_Test_3/Aufgaben_1_bis_9/test9.java
+++ src/JAVA_OCA_Test_3/Aufgaben_1_bis_9/test9.java
@@ -1,22 +1,27 @@
 /*
-Kompilierungsfehler
+Falsche Konstruktoraufruf für LocalDate:
 
-Gesamterklärung
-Der Konstruktor von LocalDate ist als privat deklariert und kann daher nicht von außen aufgerufen werden, daher new LocalDate(2020, 2, 14); Verursacht Kompilierungsfehler.
+LocalDate obj = new LocalDate(2020, 2, 14);
 
+Die Klasse LocalDate hat keinen öffentlichen Konstruktor, der Jahr, Monat und Tag als Parameter akzeptiert.
+Stattdessen wird eine statische Methode of verwendet, um eine Instanz von LocalDate zu erstellen:
 
-
-Die überladenen statischen Methoden "of" und "parse" werden bereitgestellt, um die Instanz von LocalDate zu erstellen.
-
-LocalTime, LocalDateTime, Period geben auch private Konstruktoren an und stellen die Methoden "of" und "parse" bereit, um entsprechende Instanzen zu erstellen.
- * */
+LocalDate obj = LocalDate.of(2020, 2, 14);
+* */
 package JAVA_OCA_Test_3.Aufgaben_1_bis_9;
 import java.time.LocalDate;
 import java.time.Period;
 
 //public class test9 {
 //    public static void main(String [] args) {
-//        LocalDate obj = new LocalDate(2020, 2, 14);
+//        LocalDate obj = new LocalDate(2020, 2, 14);   // <-- Syntax Fehler LocalDate.of() | ohne new
 //        System.out.println(obj.minus(Period.ofDays(10)));
 //    }
 //}
+//----------------------------------------------------------------------------------------------------------------------
+public class test9 {
+    public static void main(String [] args) {
+        LocalDate obj = LocalDate.of(2020, 2, 14);  //<-- richtige Syntax
+        System.out.println(obj.minus(Period.ofDays(10)));
+    }
+}
