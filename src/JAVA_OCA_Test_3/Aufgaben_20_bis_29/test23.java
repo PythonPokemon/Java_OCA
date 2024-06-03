34869ed54ffd61b2bf3de4ecf4d9dbd7575e3313
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-03 16:21:26 +0200

    update

--- src/JAVA_OCA_Test_3/Aufgaben_20_bis_29/test23.java
+++ src/JAVA_OCA_Test_3/Aufgaben_20_bis_29/test23.java
@@ -5,8 +5,15 @@ an.
  * */
 package JAVA_OCA_Test_3.Aufgaben_20_bis_29;
 
-public class test23 {
-    public static void main(String[] args) {
+public class test23 {   // öffentliche KLasse
+    public String color;
 
+    public test23(String color){    // öffen
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+        test23 obj = new test23("GREEN");
+        System.out.println(obj.color);
     }
 }
