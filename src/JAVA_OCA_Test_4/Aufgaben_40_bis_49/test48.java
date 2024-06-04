28069a8d4959338547a73281afb274b7b9e794aa
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-04 15:41:15 +0200

    update

--- src/JAVA_OCA_Test_4/Aufgaben_40_bis_49/test48.java
+++ src/JAVA_OCA_Test_4/Aufgaben_40_bis_49/test48.java
@@ -5,4 +5,12 @@
 package JAVA_OCA_Test_4.Aufgaben_40_bis_49;
 
 public class test48 {
+    public static void print() {
+        System.out.println("static method");// static method
+    }
+
+    public static void main(String[] args) {
+        test48 obj = null;
+        obj.print();
+    }
 }
