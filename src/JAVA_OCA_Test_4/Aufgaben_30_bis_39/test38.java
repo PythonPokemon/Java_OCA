8931f6fc02ec9bdd3fec6cd0b90b78f4edd335e5
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-04 15:29:54 +0200

    update

--- src/JAVA_OCA_Test_4/Aufgaben_30_bis_39/test38.java
+++ src/JAVA_OCA_Test_4/Aufgaben_30_bis_39/test38.java
@@ -1,6 +1,21 @@
 /*
+Given the following definitions of the class Insect and the interface Flyable,
+the task is to declare a class Mosquito that inherits from the class Insect and implements the interface Flyable.
+------------------------------------------------------------------------------------------------------------------------
+class Insect {}
+interface Flyable {}
 
+Select the correct option to accomplish this task:
+------------------------------------------------------------------------------------------------------------------------
+class Mosquito implements Insect, Flyable{}
 
+class Mosquito extends Insect, Flyable{}
+
+Correct answer
+class Mosquito extends Insect implements Flyable{}
+
+class Mosquito implements Insect extends Flyable{}
+------------------------------------------------------------------------------------------------------------------------
  * */
 package JAVA_OCA_Test_4.Aufgaben_30_bis_39;
 
--- src/JAVA_OCA_Test_4/Aufgaben_40_bis_49/test41.java
+++ src/JAVA_OCA_Test_4/Aufgaben_40_bis_49/test41.java
@@ -3,6 +3,18 @@
 
  * */
 package JAVA_OCA_Test_4.Aufgaben_40_bis_49;
+import java.util.ArrayList;
+import java.util.List;
 
 public class test41 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        List<StringBuilder> list = new ArrayList<>();
+        list.add(sb);
+        list.add(new StringBuilder("Hello"));
+        list.add(sb);
+        sb.append("World!");
+
+        System.out.println(list);   // [HelloWorld!, Hello, HelloWorld!]
+    }
 }
