be04bfebb60b2218584e2b47e8fb6d3d3422375d
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-04 16:22:22 +0200

    update

--- src/JAVA_OCA_Test_4/Aufgaben_60_bis_69/test67.java
+++ src/JAVA_OCA_Test_4/Aufgaben_60_bis_69/test67.java
@@ -5,4 +5,10 @@
 package JAVA_OCA_Test_4.Aufgaben_60_bis_69;
 
 public class test67 {
+    public static void main(String[] args) {
+        int a = 7;
+        boolean res = a++ == 7 && ++a == 9 || a++ == 9;
+        System.out.println("a = " + a); //<--- a = 9
+        System.out.println("res = " + res); //<--- res = true
+    }
 }
