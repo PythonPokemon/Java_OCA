f7b85ccb21416dc96ac5e26c62caf7bd4618f2ac
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-04 16:08:58 +0200

    update

--- src/JAVA_OCA_Test_4/Aufgaben_50_bis_59/test59.java
+++ src/JAVA_OCA_Test_4/Aufgaben_50_bis_59/test59.java
@@ -3,6 +3,20 @@
 
  * */
 package JAVA_OCA_Test_4.Aufgaben_50_bis_59;
+import java.util.ArrayList;
+import java.util.List;
 
 public class test59 {
+    public static void main(String[] args) {
+        Boolean [] arr = new Boolean[2];
+        List<Boolean> list = new ArrayList<>();
+        list.add(arr[0]);
+        list.add(arr[1]);
+
+        if(list.remove(0)) {
+            list.remove(1);
+        }
+
+        System.out.println(list);   // <--- .NullPointerException
+    }
 }
