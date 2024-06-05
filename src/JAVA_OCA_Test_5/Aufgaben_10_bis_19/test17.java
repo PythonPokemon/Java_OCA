47026f8f5a3fd672172c8d8a31c14c61385be14c
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-05 09:41:48 +0200

    update

--- src/JAVA_OCA_Test_5/Aufgaben_10_bis_19/test17.java
+++ src/JAVA_OCA_Test_5/Aufgaben_10_bis_19/test17.java
@@ -1,8 +1,52 @@
 /*
+public class main {
+    static String main = "ONE";
+
+    public main() {
+        System.out.println("TWO");
+    }
+
+    public static void main(String [] args) {
+        main();
+    }
+
+    public static void main() {
+        System.out.println(main);
+    }
+}
+------------------------------------------------------------------------------------------------------------------------
+Also consider below statements:
+
+1. Code doesn't compile
+
+2. Code compiles successfully
+
+3. Only ONE will be printed to the console
+
+4. Only TWO will be printed to the console
+
+5. Both ONE and TWO will be printed to the console
 
 
+
+How many of the above statements is/are true?
+Correct answer
+Two statements
  * */
 package JAVA_OCA_Test_5.Aufgaben_10_bis_19;
 
-public class test17 {
-}
+//public class test17 {
+//    static String main = "ONE";
+//
+//    public test17() {
+//        System.out.println("TWO");
+//    }
+//
+//    public static void main(String [] args) {
+//        main();
+//    }
+//
+//    public static void main() {
+//        System.out.println(main);
+//    }
+//}
