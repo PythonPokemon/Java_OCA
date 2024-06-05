8f9924b33d73290e89316f43b76720324326ed0b
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-05 12:01:20 +0200

    update

--- src/JAVA_OCA_Test_5/Aufgaben_50_bis_59/test56.java
+++ src/JAVA_OCA_Test_5/Aufgaben_50_bis_59/test56.java
@@ -5,4 +5,9 @@
 package JAVA_OCA_Test_5.Aufgaben_50_bis_59;
 
 public class test56 {
+    public static void main(String[] args) {
+        boolean flag1 = "Java" == "Java".replace('J', 'J'); //Line n1
+        boolean flag2 = "Java" == "Java".replace("J", "J"); //Line n2
+        System.out.println(flag1 && flag2); // true
+    }
 }
