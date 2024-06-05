924af85bb634d5a37246eefe3ea21a12d21bbe99
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-05 13:47:35 +0200

    update

---
+++ src/JAVA_OCA_Test_5/Aufgaben_60_bis_69/Profitable1.java
@@ -0,0 +1,7 @@
+package JAVA_OCA_Test_5.Aufgaben_60_bis_69;
+
+public interface Profitable1 {
+    default double profit() {
+        return 12.5;
+    }
+}
--- src/JAVA_OCA_Test_5/Aufgaben_60_bis_69/test62.java
+++ src/JAVA_OCA_Test_5/Aufgaben_60_bis_69/test62.java
@@ -4,5 +4,14 @@
  * */
 package JAVA_OCA_Test_5.Aufgaben_60_bis_69;
 
-public class test62 {
+public abstract class test62 implements Profitable1, Profitable2 {
+    public static void main(String[] args) {
+
+    }
+    /*INSERT*/
+    public double profit() {
+
+        return Profitable2.super.profit();
+
+    }
 }
