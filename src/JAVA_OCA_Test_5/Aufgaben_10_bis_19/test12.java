f5f1fc38c632aa1cb3a664fe8f91ec045c74fcd3
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-05 09:31:31 +0200

    update

--- src/JAVA_OCA_Test_5/Aufgaben_10_bis_19/test12.java
+++ src/JAVA_OCA_Test_5/Aufgaben_10_bis_19/test12.java
@@ -1,8 +1,19 @@
 /*
 
-
+FINALLY is printed to the console, stack trace is printed and then program ends abruptly
  * */
 package JAVA_OCA_Test_5.Aufgaben_10_bis_19;
 
 public class test12 {
+    private static void div() {
+        System.out.println(1/0);
+    }
+
+    public static void main(String[] args) {
+        try {
+            div();
+        } finally {
+            System.out.println("FINALLY");
+        }
+    }
 }
--- src/JAVA_OCA_Test_5/Aufgaben_10_bis_19/test14.java
+++ src/JAVA_OCA_Test_5/Aufgaben_10_bis_19/test14.java
@@ -4,5 +4,16 @@
  * */
 package JAVA_OCA_Test_5.Aufgaben_10_bis_19;
 
+class A {
+    public String toString() {
+        return null;
+    }
+}
+
 public class test14 {
+    public static void main(String [] args) {
+        String text = null;
+        text = text + new A(); //Line n1
+        System.out.println(text.length()); //Line n2    // 8
+    }
 }
