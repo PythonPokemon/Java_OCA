6396f9f2e56b668fa467ba41b184c4403e888179
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-06 11:04:52 +0200

    update

--- src/JAVA_OCA_Test_6/Aufgaben_70_bis/test70.java
+++ src/JAVA_OCA_Test_6/Aufgaben_70_bis/test70.java
@@ -4,5 +4,23 @@
  * */
 package JAVA_OCA_Test_6.Aufgaben_70_bis;
 
+interface Perishable1 {
+    default int maxDays() {
+        return 1;
+    }
+}
+
+interface Perishable2 extends Perishable1 {
+    default int maxDays() {
+        return 2;
+    }
+}
+
+class Milk implements Perishable2, Perishable1 {}
+
 public class test70 {
+    public static void main(String[] args) {
+        Perishable1 obj = new Milk();
+        System.out.println(obj.maxDays());  // 2
+    }
 }
