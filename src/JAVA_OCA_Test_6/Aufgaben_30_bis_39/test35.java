f5bda508aad19dc94d916f6d0c53281627262bc5
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-06 09:35:23 +0200

    update

--- src/JAVA_OCA_Test_6/Aufgaben_30_bis_39/test35.java
+++ src/JAVA_OCA_Test_6/Aufgaben_30_bis_39/test35.java
@@ -5,4 +5,9 @@
 package JAVA_OCA_Test_6.Aufgaben_30_bis_39;
 
 public class test35 {
+    public static void main(String[] args) {
+        String str = "PANIC";
+        StringBuilder sb = new StringBuilder("THET");
+        System.out.println(str.replace("N", sb)); //Line n1 | PATHETIC
+    }
 }
--- src/JAVA_OCA_Test_6/Aufgaben_30_bis_39/test36.java
+++ src/JAVA_OCA_Test_6/Aufgaben_30_bis_39/test36.java
@@ -4,5 +4,21 @@
  * */
 package JAVA_OCA_Test_6.Aufgaben_30_bis_39;
 
-public class test36 {
-}
+//class Paper {
+//    static String getType() { //Line n1
+//        return "GENERIC";
+//    }
+//}
+//
+//class RuledPaper extends Paper {
+//    String getType() { //Line n2 | Compilation error in RuledPaper class
+//        return "RULED";
+//    }
+//}
+//
+//public class test36 {
+//    public static void main(String[] args) {
+//        Paper paper = new RuledPaper(); //Line n3
+//        System.out.println(paper.getType()); //Line n4
+//    }
+//}
