20e3a401d563f179cc1d8bcae6c2e8115533a930
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-06 09:16:29 +0200

    update

--- src/JAVA_OCA_Test_6/Aufgaben_20_bis_29/test26.java
+++ src/JAVA_OCA_Test_6/Aufgaben_20_bis_29/test26.java
@@ -5,4 +5,8 @@
 package JAVA_OCA_Test_6.Aufgaben_20_bis_29;
 
 public class test26 {
+    public static void main(String [] args) {
+        String text = "ONE ";
+        System.out.println(text.concat(text.concat("ELEVEN ")).trim()); // ONE ONE ELEVEN
+    }
 }
