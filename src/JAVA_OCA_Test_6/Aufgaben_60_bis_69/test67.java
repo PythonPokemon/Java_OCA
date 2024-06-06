a2b35ace4316a7c437e400ac0c21303f44795bd2
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-06 11:01:40 +0200

    update

--- src/JAVA_OCA_Test_6/Aufgaben_60_bis_69/test67.java
+++ src/JAVA_OCA_Test_6/Aufgaben_60_bis_69/test67.java
@@ -4,5 +4,22 @@
  * */
 package JAVA_OCA_Test_6.Aufgaben_60_bis_69;
 
-public class test67 {
-}
+//class Gamma {
+//    Gamma() {
+//        System.out.print("Reach");
+//    }
+//}
+//
+//class Gam extends Gamma {
+//    Gam() {
+//        Super();
+//        System.out.print("Out");    // Compilation Error
+//    }
+//}
+//
+//
+//public class test67 {
+//    public static void main(String[] args) {
+//        new Gam();
+//    }
+//}
