ea56a916afab4d8b5d4051865560f9cb7068abd8
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-15 17:32:19 +0200

    update

---
+++ src/JAVA_Insel_Smart/Kapitel_2/Schleifen/For_Schleife.java
@@ -0,0 +1,15 @@
+/*
+Die for-Schleife
+
+Die for-Schleife ist besonders nützlich, wenn Sie eine Schleife mit einer festen Anzahl von Iterationen benötigen:
+* */
+package JAVA_Insel_Smart.Kapitel_2.Schleifen;
+
+public class For_Schleife {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
