02eddd96fe90b062cf3451e4ef262c3d60211f65
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-17 20:29:15 +0200

    update

---
+++ src/JAVA_Insel_Smart/Kapitel_4/Erweiterte_Schleife.java
@@ -0,0 +1,12 @@
+package JAVA_Insel_Smart.Kapitel_4;
+
+public class Erweiterte_Schleife {
+    public static void main(String[] args) {
+        int[] numbers = { 2, 3, 5, 7, 11, 13 };
+
+        for (int number : numbers) {    // number iteriert durch numbers, von element zu element
+            System.out.println(number); // jedes Mal, wenn number durch ein neues element iteriert, wird es ausgegeben
+        }
+
+    }
+}
