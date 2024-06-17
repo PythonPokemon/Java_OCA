9bb0986abf0ae492c389d3ef5d87c7eb79f259e4
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-17 20:39:22 +0200

    update

---
+++ src/JAVA_Insel_Smart/Kapitel_4/Array_mit_StringElement.java
@@ -0,0 +1,16 @@
+/*
+Arrays mit nichtprimitiven Elementen
+Arrays können auch nichtprimitive Elemente enthalten, z.B. Objekte:
+* */
+
+package JAVA_Insel_Smart.Kapitel_4;
+
+public class Array_mit_StringElement {
+    public static void main(String[] args) {
+        String[] words = {"Hello", "World"};
+
+        for (String word : words) { // word iteriert durch words elemente
+            System.out.println(word);   // jedes element, durch das iteriert wird, wird in der Konsole ausgegeben
+        }
+    }
+}
