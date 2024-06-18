e9a52c73587b9ed678565d3cb90eb374237ce0d2
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-18 13:52:17 +0200

    update

---
+++ src/JAVA_Insel_Smart/Kapitel_5/Zeichen_zur_Zahl_Konvertieren.java
@@ -0,0 +1,9 @@
+/*
+Von char in int: vom Zeichen zur Zahl
+Die Character-Klasse bietet Methoden zur Umwandlung von Zeichen in ihren numerischen Wert und umgekehrt.
+* */
+
+package JAVA_Insel_Smart.Kapitel_5;
+
+public class Zeichen_zur_Zahl_Konvertieren {
+}
