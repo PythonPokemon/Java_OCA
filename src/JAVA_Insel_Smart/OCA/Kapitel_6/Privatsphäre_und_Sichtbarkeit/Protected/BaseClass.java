b1c3f20e22f842290cd074d0096d3e41cf485919
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-21 11:04:01 +0200

    update

--- src/JAVA_Insel_Smart/OCA/Kapitel_6/Privatsphäre_und_Sichtbarkeit/Protected/BaseClass.java
+++ src/JAVA_Insel_Smart/OCA/Kapitel_6/Privatsphäre_und_Sichtbarkeit/Protected/BaseClass.java
@@ -6,8 +6,13 @@ package JAVA_Insel_Smart.OCA.Kapitel_6.Privatsphäre_und_Sichtbarkeit.Protected;
 
 // Beispiel 1: Zugriff innerhalb desselben Pakets
 public class BaseClass {
-    protected void geschuetzeMethode() {
 
+    protected void geschuetzeMethode() {
         System.out.println("Geschütz Methode aufgerufen");
     }
+
+    protected static void geschuetzStatischeMethode () {
+        System.out.println("Geschütze statische Methode aufgerufen");
+    }
+
 }
--- src/JAVA_Insel_Smart/OCA/Kapitel_6/Privatsphäre_und_Sichtbarkeit/Protected/SamePackageClass.java
+++ src/JAVA_Insel_Smart/OCA/Kapitel_6/Privatsphäre_und_Sichtbarkeit/Protected/SamePackageClass.java
@@ -3,9 +3,18 @@ package JAVA_Insel_Smart.OCA.Kapitel_6.Privatsphäre_und_Sichtbarkeit.Protected;
 public class SamePackageClass {
     public static void main(String[] args) {
 
-// erstellung der ObjektInstanz für die Klasse 'BaseClass' pflicht, weil nicht statisch!
+        // erstellung der ObjektInstanz für die Klasse 'BaseClass' pflicht, weil nicht statisch!
         BaseClass objektInstanzDummy1 = new BaseClass();
+        // Die Instanz der Klasse, ruft die methode in der Klasse auf.
         objektInstanzDummy1.geschuetzeMethode(); //
+
+        // das kann man so oft wiederholen so lange der speicher reicht XD | fast wie beim Sonderschlussverkauf
+        BaseClass objektInstanzDummy2 = new BaseClass();
+        objektInstanzDummy2.geschuetzeMethode();
+
+        // Aufruf der Methode direkt über den Klassen namen,
+        // da die methode statisch ist, braucht man kein Objekt_Instanz_Dummy
+        BaseClass.geschuetzStatischeMethode();
 
     }
 }
