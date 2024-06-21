f9aec4631e97a9ef3aa0d4cda9889627660c67f3
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-21 09:25:00 +0200

    update

--- src/JAVA_Insel_Smart/OCA/Kapitel_6/Statische_Konstanten_Aufzählungen.java
+++ src/JAVA_Insel_Smart/OCA/Kapitel_6/Statische_Konstanten_Aufzählungen.java
@@ -1,4 +1,18 @@
+/*
+Konstanten über statische finale Variablen
+Konstanten sind Variablen, deren Wert nach der Initialisierung nicht mehr geändert werden kann.
+Sie werden mit den Schlüsselwörtern static und final deklariert.
+* */
+
 package JAVA_Insel_Smart.OCA.Kapitel_6;
 
+class Spiel {
+    // Konstante werden mit dem Schlüsselwort static und final deklariert
+    static final int Maximale_Spieler = 10;
+}
+
 public class Statische_Konstanten_Aufzählungen {
+    public static void main(String[] args) {
+
+    }
 }
