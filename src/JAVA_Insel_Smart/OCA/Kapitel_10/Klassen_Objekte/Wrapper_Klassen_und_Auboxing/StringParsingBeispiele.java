b769289eb721e74f06f8dbc36aba0dc8c2957650
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-06-27 11:49:03 +0200

    update

---
+++ src/JAVA_Insel_Smart/OCA/Kapitel_10/Klassen_Objekte/Wrapper_Klassen_und_Auboxing/StringParsingBeispiele.java
@@ -0,0 +1,31 @@
+/*
+Erklärung
+------------------------------------------------------------------------------------------------------------------------
+Parsen von Strings in primitive Typen:
+
+Integer.parseInt("42") konvertiert den String "42" in den int-Wert 42.
+Double.parseDouble("3.14") konvertiert den String "3.14" in den double-Wert 3.14.
+Boolean.parseBoolean("true") konvertiert den String "true" in den boolean-Wert true.
+------------------------------------------------------------------------------------------------------------------------
+Ausgabe der geparsten Werte:
+
+System.out.println("Geparster int-Wert: " + i);
+System.out.println("Geparster double-Wert: " + d);
+System.out.println("Geparster boolean-Wert: " + b);
+------------------------------------------------------------------------------------------------------------------------
+* */
+package JAVA_Insel_Smart.OCA.Kapitel_10.Klassen_Objekte.Wrapper_Klassen_und_Auboxing;
+
+public class StringParsingBeispiele {
+    public static void main(String[] args) {
+        // Parsen von String in primitive Typen
+        int i = Integer.parseInt("42");
+        double d = Double.parseDouble("3.14");
+        boolean b = Boolean.parseBoolean("true");
+
+        // Ausgabe der geparsten Werte
+        System.out.println("Geparster int-Wert: " + i); // 42
+        System.out.println("Geparster double-Wert: " + d); // 3.14
+        System.out.println("Geparster boolean-Wert: " + b); // true
+    }
+}
---
+++ src/JAVA_Insel_Smart/OCA/Kapitel_10/Klassen_Objekte/Wrapper_Klassen_und_Auboxing/WrapperToStringBeispiele.java
@@ -0,0 +1,44 @@
+/*
+Erklärung
+------------------------------------------------------------------------------------------------------------------------
+Erzeugen von Wrapper-Objekten: Wrapper-Objekte werden erstellt, um primitive Werte zu speichern.
+
+Integer i = 42;
+Double d = 3.14;
+Boolean b = true;
+------------------------------------------------------------------------------------------------------------------------
+Konvertierung in String-Repräsentation: Die toString-Methode der Wrapper-Klassen wird verwendet,
+um die enthaltenen Werte in Strings zu konvertieren.
+
+String intString = i.toString();
+String doubleString = d.toString();
+String booleanString = b.toString();
+------------------------------------------------------------------------------------------------------------------------
+Ausgabe der String-Repräsentationen: Die String-Repräsentationen werden ausgegeben.
+
+System.out.println("Integer als String: " + intString);
+System.out.println("Double als String: " + doubleString);
+System.out.println("Boolean als String: " + booleanString);
+------------------------------------------------------------------------------------------------------------------------
+* */
+
+package JAVA_Insel_Smart.OCA.Kapitel_10.Klassen_Objekte.Wrapper_Klassen_und_Auboxing;
+
+public class WrapperToStringBeispiele {
+    public static void main(String[] args) {
+        // Erzeugen von Wrapper-Objekten
+        Integer i = 42;
+        Double d = 3.14;
+        Boolean b = true;
+
+        // Konvertierung in String-Repräsentation
+        String intString = i.toString(); // "42"
+        String doubleString = d.toString(); // "3.14"
+        String booleanString = b.toString(); // "true"
+
+        // Ausgabe der String-Repräsentationen
+        System.out.println("Integer als String: " + intString);
+        System.out.println("Double als String: " + doubleString);
+        System.out.println("Boolean als String: " + booleanString);
+    }
+}
