737619bdcc8331e2947b8360b50d60c6567b27af
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-07-02 12:47:21 +0200

    update

--- src/JAVA_Insel_Smart/OCA/Kapitel_1/Umwandlung_Casting/Implizite_Umwandlung_Widening.java
+++ src/JAVA_Insel_Smart/OCA/Kapitel_1/Umwandlung_Casting/Implizite_Umwandlung_Widening.java
@@ -7,6 +7,8 @@ char zu int, long, float, double
 int zu long, float, double
 long zu float, double
 float zu double
+------------------------------------------------------------------------------------------------------------------------
+also kann man implizit gar nicht ein int zum char machen nur explizit!
 * */
 package JAVA_Insel_Smart.OCA.Kapitel_1.Umwandlung_Casting;
 
@@ -22,32 +24,27 @@ public class Implizite_Umwandlung_Widening {
         System.out.println("short zu int: " + i);
 //----------------------------------------------------------------------------------------------------------------------
         long l = i;  // implizite Umwandlung von int zu long
+        double d3 = i;  // implizite Umwandlung von int zu double
         System.out.println("int zu long: " + l);
+        System.out.println("int zu double: " + d3);
 //----------------------------------------------------------------------------------------------------------------------
         float f = l;  // implizite Umwandlung von long zu float
+        double d4 = l;  // implizite Umwandlung von long zu double
         System.out.println("long zu float: " + f);
+        System.out.println("long zu double: " + d4);
 //----------------------------------------------------------------------------------------------------------------------
         double d = f;  // implizite Umwandlung von float zu double
         System.out.println("float zu double: " + d);
 //----------------------------------------------------------------------------------------------------------------------
-        char charValue = 'A';
-        int intFromChar = charValue;  // implizite Umwandlung von char zu int
-        System.out.println("char zu int: " + intFromChar);
-//----------------------------------------------------------------------------------------------------------------------
-        long longFromChar = charValue;  // implizite Umwandlung von char zu long
-        System.out.println("char zu long: " + longFromChar);
-//----------------------------------------------------------------------------------------------------------------------
-        float floatFromChar = charValue;  // implizite Umwandlung von char zu float
-        System.out.println("char zu float: " + floatFromChar);
-//----------------------------------------------------------------------------------------------------------------------
-        double doubleFromChar = charValue;  // implizite Umwandlung von char zu double
-        System.out.println("char zu double: " + doubleFromChar);
-//----------------------------------------------------------------------------------------------------------------------
-        // Zusätzliche Umwandlungen für Vollständigkeit
-        double anotherDoubleValue = i;  // implizite Umwandlung von int zu double
-        System.out.println("int zu double: " + anotherDoubleValue);
-//----------------------------------------------------------------------------------------------------------------------
-        double anotherDoubleValueFromLong = l;  // implizite Umwandlung von long zu double
-        System.out.println("long zu double: " + anotherDoubleValueFromLong);
+        char c = 'A'; // 62 weil Char 'A' ASCI / UNI-Code den Platz 65 entspricht
+
+        int i2 = c;  // implizite Umwandlung von char zu int
+        long l2 = c;  // implizite Umwandlung von char zu long
+        float f2 = c;  // implizite Umwandlung von char zu float
+        double d2 = c;  // implizite Umwandlung von char zu double
+        System.out.println("char zu int: " + i2);
+        System.out.println("char zu long: " + l2);
+        System.out.println("char zu float: " + f2);
+        System.out.println("char zu double: " + d2);
     }
 }
