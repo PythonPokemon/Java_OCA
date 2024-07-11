f8fbf7af65607f16ddf82f97c06261916ab70fd8
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-07-11 09:15:53 +0200

    update

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,18 @@
-
 public class Test {
     public static void main(String[] args) {
-        System.out.println();
+        int[] a = { 1, 2, 3, 4 };
+
+        int[] b = { 2, 3, 1, 0 };
+
+        System.out.println( a[ (a = b)[2] ] ); // also wenn man den index hier im ausdruck verändert
+
+
+       // System.out.println( a[ b[1] ] );
+       // System.out.println( a[ 3 ] );
+
+//        System.out.println(b[0]);
+//        System.out.println(b[1]);
+
+
     }
 }
