ff6931ff20ad6a52eef746c4ebc2bcffc053791b
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-07-15 22:37:01 +0200

    update

--- src/JAVA_Insel_Smart/OCA/Kapitel_3/GleicheObjekte_GleicherInhalt.java
+++ src/JAVA_Insel_Smart/OCA/Kapitel_3/GleicheObjekte_GleicherInhalt.java
@@ -19,7 +19,10 @@ public class GleicheObjekte_GleicherInhalt {
         Point p = new Point( 10, 10 );
         Point q = new Point( 10, 10 );
 
-        System.out.println( p == q ); // false
+        System.out.println( p == q ); // false, da verglichen wird ob beide referenvariablen zur gleichen
+                                      // speicheradresse des Objekt führe, aber es sind zwei unterschiedliche Objekte
+        System.out.println(p.hashCode());
+        System.out.println(q.hashCode());
         System.out.println( p.equals(q) ); // true. Da symmetrisch auch q.equals(p)
         //Nur equals(…) testet in diesem Fall die inhaltliche Gleichheit.
     }
