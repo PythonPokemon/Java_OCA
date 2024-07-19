a531998a3514be4abb50f599e7d4e7e5cdd5c75f
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-07-19 09:16:14 +0200

    update

--- src/JAVA_Insel_Smart/OCA/Datum_und_Zeitwerte/LocalDateTime/LocalDateTimeExample.java
+++ src/JAVA_Insel_Smart/OCA/Datum_und_Zeitwerte/LocalDateTime/LocalDateTimeExample.java
@@ -16,5 +16,38 @@ getMinute()
 
 package JAVA_Insel_Smart.OCA.Datum_und_Zeitwerte.LocalDateTime;
 
+import java.time.LocalDateTime;
+
 public class LocalDateTimeExample {
+    public static void main(String[] args) {
+        // Erzeugt das aktuelle Datum und Uhrzeit
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        System.out.println("Aktuelles Datum und Uhrzeit: " + currentDateTime);
+
+        // Erzeugt ein bestimmtes Datum und Uhrzeit
+        LocalDateTime specificDateTime = LocalDateTime.of(2020, 5, 20, 14, 30);
+        System.out.println("Spezielles Datum und Uhrzeit: " + specificDateTime);
+
+        // Parse ein Datum und Uhrzeit von einem String
+        LocalDateTime parsedDateTime = LocalDateTime.parse("2023-07-08T10:15:30");
+        System.out.println("Geparstes Datum und Uhrzeit: " + parsedDateTime);
+
+        // Hinzufügen von Tagen
+        LocalDateTime dateTimePlusDays = currentDateTime.plusDays(10);
+        System.out.println("Datum und Uhrzeit plus 10 Tage: " + dateTimePlusDays);
+
+        // Subtrahieren von Tagen
+        LocalDateTime dateTimeMinusDays = currentDateTime.minusDays(10);
+        System.out.println("Datum und Uhrzeit minus 10 Tage: " + dateTimeMinusDays);
+
+        // Abrufen von Tag, Monat, Jahr, Stunde und Minute
+        int day = currentDateTime.getDayOfMonth();
+        int month = currentDateTime.getMonthValue();
+        int year = currentDateTime.getYear();
+        int hour = currentDateTime.getHour();
+        int minute = currentDateTime.getMinute();
+        System.out.println("Tag: " + day + ", Monat: " + month + ", Jahr: " + year);
+        System.out.println("Stunde: " + hour + ", Minute: " + minute);
+    }
 }
+
