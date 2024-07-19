47ba5c15e70c04930c6d1c1ed56223c652ce6853
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-07-19 18:03:15 +0200

    update

--- src/JAVA_Insel_Smart/OCA/Zugriffs_Modifizierer/a/SamePackageAccess.java
+++ src/JAVA_Insel_Smart/OCA/Zugriffs_Modifizierer/a/SamePackageAccess.java
@@ -1,18 +1,18 @@
 package JAVA_Insel_Smart.OCA.Zugriffs_Modifizierer.a;
 
 public class SamePackageAccess {
-//    public static void main(String[] args) {
-//        AccessTest obj = new AccessTest();
-//
-//        // Zugriff auf verschiedene Mitglieder
-//        obj.packagePrivateDefaultVar = 1; // Erlaubt
-//        // obj.privateVar = 2; // Nicht erlaubt (Kompilierfehler)
-//        obj.protectedVar = 3; // Erlaubt
-//        obj.publicVar = 4; // Erlaubt
-//
-//        obj.packagePrivateMethod(); // Erlaubt
-//        // obj.privateMethod(); // Nicht erlaubt (Kompilierfehler)
-//        obj.protectedMethod(); // Erlaubt
-//        obj.publicMethod(); // Erlaubt
-//    }
+    public static void main(String[] args) {
+        AccessTest obj = new AccessTest();
+
+        // Zugriff auf verschiedene Mitglieder
+        obj.packagePrivateDefaultVar = 1; // Erlaubt
+        // obj.privateVar = 2; // Nicht erlaubt (Kompilierfehler)
+        obj.protectedVar = 3; // Erlaubt
+        obj.publicVar = 4; // Erlaubt
+
+        obj.packagePrivateMethod(); // Erlaubt
+        // obj.privateMethod(); // Nicht erlaubt (Kompilierfehler)
+        obj.protectedMethod(); // Erlaubt
+        obj.publicMethod(); // Erlaubt
+    }
 }
