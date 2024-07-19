3a1586b6bd95387c71457c2ece7b5074006a6b90
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-07-19 17:16:10 +0200

    update

--- src/JAVA_Insel_Smart/OCA/Zugriffs_Modifizierer/a/AccessTest.java
+++ src/JAVA_Insel_Smart/OCA/Zugriffs_Modifizierer/a/AccessTest.java
@@ -1,8 +1,20 @@
 package JAVA_Insel_Smart.OCA.Zugriffs_Modifizierer.a;
 
 public class AccessTest {
-    int a; // Default (package-private) Zugriffsmodifizierer
-    private int b; // Private Zugriffsmodifizierer
-    protected void c() {} // Protected Zugriffsmodifizierer
-    public int d() { return 0; } // Public Zugriffsmodifizierer
+    // Fields | Variablen
+    int packagePrivateDefaultVar; // package-private
+    private String privateVar; // private
+    protected int protectedVar; // protected
+    public int publicVar; // public
+
+    // Fields | Methoden
+    void packagePrivateMethod() {} // package-private
+    private void privateMethod() {} // private
+    protected void protectedMethod() {} // protected
+    public void publicMethod() {} // public
+
+    public static void main(String[] args) {
+        AccessTest objRef = new AccessTest();
+        System.out.println(objRef.privateVar);
+    }
 }
